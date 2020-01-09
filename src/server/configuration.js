59b967ee4fc97ada13501f7447f95b6708a75090
John Wregglesworth <wregglej@cyverse.org>
2020-01-09 10:12:57 -0700

    Fix up the JSDoc for configuration.js

--- src/server/configuration.js
+++ src/server/configuration.js
@@ -1,9 +1,15 @@
+/**
+ * Application configuration module.
+ * @module configuration
+ */
+
 import config from "config";
 
 /**
  * Ensures that the values from the configuration file and the environment
  * variables are generally acceptable. Accepts no parameters. Throws an
  * exception if there is a problem.
+ * @function
  */
 export const validate = () => {
     if (!config.has("terrain_url")) {
@@ -12,20 +18,26 @@ export const validate = () => {
 };
 
 /**
- * @type {string} - The base URL for the Terrain service. Taken from the
+ * The base URL for the Terrain service. Taken from the
  * 'terrain_url' setting in the configuration file.
+ *
+ * @type {string}
  */
 export const terrainURL = config.get("terrain_url");
 
 /**
- * @type {boolean} - Is set to true if the application is running in
+ * Is set to true if the application is running in
  * development mode. Checks to make sure the NODE_ENV environment variable
  * is not set to 'production'.
+ *
+ * @type {boolean}
  */
 export const isDevelopment = process.env.NODE_ENV !== "production";
 
 /**
- * @type {number} - The port the application listens on for requests. Takes its
+ * The port the application listens on for requests. Takes its
  * value from the PORT environment variable. Defaults to 3000 if it's not set.
+ *
+ * @type {number}
  */
 export const listenPort = parseInt(process.env.PORT || "3000", 10);

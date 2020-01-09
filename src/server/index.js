7e11a82bcd9c1350fa4eda1ba811136362274f58
John Wregglesworth <wregglej@cyverse.org>
2020-01-09 08:52:12 -0700

    Apply prettier formatting to server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import next from "next";
 
-import { ApolloServer } from "apollo-server-express";
-import { applyMiddleware } from "graphql-middleware";
-import { makeExecutableSchema } from "graphql-tools";
+import {
+    ApolloServer
+} from "apollo-server-express";
+import {
+    applyMiddleware
+} from "graphql-middleware";
+import {
+    makeExecutableSchema
+} from "graphql-tools";
 
 import typeDefs from "./graphql/typesDefs";
 import resolvers from "./graphql/resolvers";
@@ -13,10 +19,15 @@ import TerrainDataSource from "./graphql/dataSources/TerrainDataSource";
 const dev = process.env.NODE_ENV !== "production";
 const port = parseInt(process.env.PORT || "3000", 10);
 
-const app = next({ dev });
+const app = next({
+    dev
+});
 const nextHandler = app.getRequestHandler();
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
 const schemaWithMiddleware = applyMiddleware(schema, camelCaseMiddleware);
 
 const apolloServer = new ApolloServer({
@@ -32,7 +43,9 @@ app.prepare()
         const server = express();
 
         // Add Apollo as middleware to Express.
-        apolloServer.applyMiddleware({ app: server });
+        apolloServer.applyMiddleware({
+            app: server
+        });
 
         server.get("*", (req, res) => {
             return nextHandler(req, res);

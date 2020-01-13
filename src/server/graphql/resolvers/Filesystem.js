80576f98eed7dc17f7f439b410225d7247b3dcf3
John Wregglesworth <wregglej@cyverse.org>
2020-01-13 10:39:43 -0700

    Add enum mappings in the resolvers for the Filesystem stuff

---
+++ src/server/graphql/resolvers/Filesystem.js
@@ -0,0 +1,31 @@
+export default {
+    // Mapping for the SortColumn enum.
+    SortColumn: {
+        NAME: "NAME",
+        ID: "ID",
+        LASTMODIFIED: "LASTMODIFIED",
+        DATECREATED: "DATECREATED",
+        SIZE: "SIZE",
+        PATH: "PATH",
+    },
+
+    // Mapping for the EntityType enum.
+    EntityType: {
+        FILE: "FILE",
+        FOLDER: "FOLDER",
+        ANY: "ANY",
+    },
+
+    // Mapping for the SortDirection enum.
+    SortDirection: {
+        ASC: "ASC",
+        DESC: "DESC",
+    },
+
+    // Mapping for the Permission enum.
+    Permission: {
+        READ: "read",
+        WRITE: "write",
+        OWN: "own",
+    },
+};

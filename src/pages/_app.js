6a8577527698d5ec057ec07d2814745a5e58839a
Sarah Roberts <sarahr@cyverse.org>
2020-01-16 14:17:07 -0700

    CORE-868: allow the user to click the "user" button in the navigation bar in order to log in

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,35 @@ import App from "next/app";
 import CyverseAppBar from "../components/appBar/CyVerseAppBar";
 
 export default class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        if (ctx.req && ctx.req.user) {
+            pageProps.user = ctx.req.user.profile;
+        }
+        return { pageProps };
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: props.pageProps.user,
+        };
+    }
+
     render() {
         const { Component, pageProps } = this.props;
 
+        const props = {
+            ...pageProps,
+            user: this.state.user,
+        };
+
         return (
             <CyverseAppBar>
-                <Component {...pageProps} />
+                <Component {...props} />
             </CyverseAppBar>
         );
     }

52c767a45a2017497840f8c721e8be343a4ecda1
John Wregglesworth <wregglej@iplantcollaborative.org>
2020-01-30 12:44:23 -0700

    Reorganize code to keep things readable and prevent useStyle() overuse

--- src/components/UploadDialog/index.js
+++ src/components/UploadDialog/index.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 import AddCircleIcon from "@material-ui/icons/AddCircle";
-//import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
@@ -12,7 +11,6 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Divider from "@material-ui/core/Divider";
-//import Grid from "@material-ui/core/Grid";
 import HttpOutlinedIcon from "@material-ui/icons/HttpOutlined";
 import IconButton from "@material-ui/core/IconButton";
 import InsertDriveFileOutlinedIcon from "@material-ui/icons/InsertDriveFileOutlined";
@@ -24,7 +22,6 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListSubheader from "@material-ui/core/ListSubheader";
-//import Paper from "@material-ui/core/Paper";
 import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
 import TextField from "@material-ui/core/TextField";
 import { Typography } from "@material-ui/core";
@@ -33,9 +30,6 @@ const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
-    // browseButton: {
-    //     backgroundColor: theme.palette.primary,
-    // },
     browseButtonContainer: {
         display: "flex",
         justifyContent: "center",
@@ -81,6 +75,123 @@ const UploadDialog = (props) => {
     const classes = useStyles();
     const { destination } = props;
 
+    const UploadList = () => {
+        return (
+            <List
+                subheader={
+                    <ListSubheader className={classes.listSubheader}>
+                        Files to upload
+                    </ListSubheader>
+                }
+                className={classes.fileList}
+            >
+                <ListItem>
+                    <ListItemIcon>
+                        <InsertDriveFileOutlinedIcon />
+                    </ListItemIcon>
+
+                    <ListItemText id="test-filename" primary="test-filename" />
+
+                    <ListItemSecondaryAction>
+                        <IconButton aria-label="remove">
+                            <RemoveCircleIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+
+                <Divider variant="inset" />
+
+                <ListItem>
+                    <ListItemIcon>
+                        <HttpOutlinedIcon />
+                    </ListItemIcon>
+
+                    <ListItemText
+                        id="test-URL"
+                        primary="http://de.cyverse.org/terrain/docs"
+                    />
+
+                    <ListItemSecondaryAction>
+                        <IconButton aria-label="remove">
+                            <RemoveCircleIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+            </List>
+        );
+    };
+
+    const URLImportTextField = () => {
+        return (
+            <div className={classes.textFieldContainer}>
+                <TextField
+                    id="url-text-field"
+                    label="Add URL to Import"
+                    className={classes.textField}
+                    variant="outlined"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <HttpOutlinedIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <IconButton aria-label="add-url-import">
+                    <AddCircleIcon />
+                </IconButton>
+            </div>
+        );
+    };
+
+    const UploadCard = () => {
+        return (
+            <Card variant="outlined" className={classes.uploadCard}>
+                <CardContent style={{ height: "100%" }}>
+                    <Typography
+                        align="center"
+                        className={classes.uploadCardTypography}
+                        paragraph={true}
+                        variant="body1"
+                    >
+                        Drop files or URLs here to queue them for upload to the
+                        data store
+                    </Typography>
+
+                    <Typography
+                        align="center"
+                        className={classes.uploadCardTypography}
+                        paragraph={true}
+                        variant="body1"
+                    >
+                        or
+                    </Typography>
+
+                    <div className={classes.browseButtonContainer}>
+                        <Button variant="contained" color="primary">
+                            Browse
+                        </Button>
+                    </div>
+
+                    <Typography
+                        align="center"
+                        className={classes.uploadCardTypography}
+                        paragraph={true}
+                        variant="caption"
+                    >
+                        Manual upload with third-party apps: &nbsp;
+                        <Link
+                            href="#"
+                            onClick={(event) => event.preventDefault()}
+                        >
+                            Learn more.
+                        </Link>
+                    </Typography>
+                </CardContent>
+            </Card>
+        );
+    };
+
     const handleClose = () => {
         setOpen(false);
     };
@@ -100,113 +211,11 @@ const UploadDialog = (props) => {
                 </DialogTitle>
 
                 <DialogContent className={classes.root}>
-                    <Card variant="outlined" className={classes.uploadCard}>
-                        <CardContent style={{ height: "100%" }}>
-                            <Typography
-                                align="center"
-                                className={classes.uploadCardTypography}
-                                paragraph={true}
-                                variant="body1"
-                            >
-                                Drop files or URLs here to queue them for upload
-                                to the data store
-                            </Typography>
+                    <UploadCard />
 
-                            <Typography
-                                align="center"
-                                className={classes.uploadCardTypography}
-                                paragraph={true}
-                                variant="body1"
-                            >
-                                or
-                            </Typography>
+                    <URLImportTextField />
 
-                            <div className={classes.browseButtonContainer}>
-                                <Button variant="contained" color="primary">
-                                    Browse
-                                </Button>
-                            </div>
-
-                            <Typography
-                                align="center"
-                                className={classes.uploadCardTypography}
-                                paragraph={true}
-                                variant="caption"
-                            >
-                                Manual upload with third-party apps: &nbsp;
-                                <Link
-                                    href="#"
-                                    onClick={(event) => event.preventDefault()}
-                                >
-                                    Learn more.
-                                </Link>
-                            </Typography>
-                        </CardContent>
-                    </Card>
-
-                    <div className={classes.textFieldContainer}>
-                        <TextField
-                            id="url-text-field"
-                            label="Add URL to Import"
-                            className={classes.textField}
-                            variant="outlined"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <HttpOutlinedIcon />
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                        <IconButton aria-label="add-url-import">
-                            <AddCircleIcon />
-                        </IconButton>
-                    </div>
-
-                    <List
-                        subheader={
-                            <ListSubheader className={classes.listSubheader}>
-                                Files to upload
-                            </ListSubheader>
-                        }
-                        className={classes.fileList}
-                    >
-                        <ListItem>
-                            <ListItemIcon>
-                                <InsertDriveFileOutlinedIcon />
-                            </ListItemIcon>
-
-                            <ListItemText
-                                id="test-filename"
-                                primary="test-filename"
-                            />
-
-                            <ListItemSecondaryAction>
-                                <IconButton aria-label="remove">
-                                    <RemoveCircleIcon />
-                                </IconButton>
-                            </ListItemSecondaryAction>
-                        </ListItem>
-
-                        <Divider variant="inset" />
-
-                        <ListItem>
-                            <ListItemIcon>
-                                <HttpOutlinedIcon />
-                            </ListItemIcon>
-
-                            <ListItemText
-                                id="test-URL"
-                                primary="http://de.cyverse.org/terrain/docs"
-                            />
-
-                            <ListItemSecondaryAction>
-                                <IconButton aria-label="remove">
-                                    <RemoveCircleIcon />
-                                </IconButton>
-                            </ListItemSecondaryAction>
-                        </ListItem>
-                    </List>
+                    <UploadList />
                 </DialogContent>
 
                 <DialogActions className={classes.uploadDialogActions}>
@@ -217,6 +226,7 @@ const UploadDialog = (props) => {
                     >
                         Cancel
                     </Button>
+
                     <Button
                         onClick={handleClose}
                         color="primary"

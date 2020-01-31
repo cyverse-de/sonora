44f89e688f37323bbbccf217dfda12861bc84203
Ashley Ramsey <aramsey@cyverse.org>
2020-01-30 17:57:30 -0700

    Add data page with path query param

---
+++ src/pages/data.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { useRouter } from "next/router";
+import Listing from "../components/data/Listing";
+
+export default function Data() {
+    const router = useRouter();
+    let path = router?.query?.path;
+
+    // Remove any trailing slashes
+    if (path) {
+        path = path.replace(/\/+$/, "");
+    }
+
+    return <Listing path={path} />;
+}

c7cf157ddf1f452f58aebfd5176aa6bea11cebc2
Ashley Ramsey <aramsey@cyverse.org>
2020-01-30 17:33:36 -0700

    Refactor _app to function and wrap withApolloClient
    Since we don't need getInitialProps anymore, we can simplify the code to a function

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,42 +1,18 @@
 import React from "react";
-import App from "next/app";
 import CyverseAppBar from "../components/appBar/CyVerseAppBar";
 import theme from "../components/theme/default";
 import { ThemeProvider } from "@material-ui/core/styles";
 
-export default class MyApp extends App {
-    static async getInitialProps({ Component, ctx }) {
-        let pageProps = {};
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-        if (ctx.req && ctx.req.user) {
-            pageProps.user = ctx.req.user.profile;
-        }
-        return { pageProps };
-    }
+import withApolloClient from "../withApolloClient";
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: props.pageProps.user,
-        };
-    }
-
-    render() {
-        const { Component, pageProps } = this.props;
-
-        const props = {
-            ...pageProps,
-            user: this.state.user,
-        };
-
-        return (
-            <ThemeProvider theme={theme}>
-                <CyverseAppBar>
-                    <Component {...props} />
-                </CyverseAppBar>
-            </ThemeProvider>
-        );
-    }
+function MyApp({ Component, pageProps }) {
+    return (
+        <ThemeProvider theme={theme}>
+            <CyverseAppBar>
+                <Component {...pageProps} />
+            </CyverseAppBar>
+        </ThemeProvider>
+    );
 }
+
+export default withApolloClient(MyApp);

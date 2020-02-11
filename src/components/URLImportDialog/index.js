53c11379482f2d3113b34c6b39cee627ed9f7655
John Wregglesworth <wregglej@iplantcollaborative.org>
2020-02-11 16:22:17 -0700

    Fix text field margins on mobile

--- src/components/URLImportDialog/index.js
+++ src/components/URLImportDialog/index.js
@@ -44,6 +44,10 @@ const useStyles = makeStyles((theme) => ({
     urlField: {
         width: "100%",
         margin: "0 10px 0 0",
+
+        [theme.breakpoints.down("sm")]: {
+            margin: 0,
+        },
     },
     closeDialog: {
         position: "absolute",

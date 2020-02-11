e0367c9946c41afcdf96d8ec74ddc86db3655589
sriram srinivasan <sriram@cyverse.org>
2020-02-10 21:27:23 -0500

    fix prettier command and run it once

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,33 +4,36 @@
  * Copied from https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js
  */
 
-import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
 import theme from "../components/theme/default";
 
 export default class MyDocument extends Document {
     render() {
         return (
             <html lang="en">
-            <Head>
-                {/* PWA primary color */}
-                <meta name="theme-color" content={theme.palette.primary.main} />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                />
-            </Head>
-            <body>
-            <Main />
-            <NextScript />
-            </body>
+                <Head>
+                    {/* PWA primary color */}
+                    <meta
+                        name="theme-color"
+                        content={theme.palette.primary.main}
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
             </html>
         );
     }
 }
 
-MyDocument.getInitialProps = async ctx => {
+MyDocument.getInitialProps = async (ctx) => {
     // Resolution order
     //
     // On the server:
@@ -59,7 +62,7 @@ MyDocument.getInitialProps = async ctx => {
 
     ctx.renderPage = () =>
         originalRenderPage({
-            enhanceApp: App => props => sheets.collect(<App {...props} />),
+            enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
         });
 
     const initialProps = await Document.getInitialProps(ctx);
@@ -67,6 +70,9 @@ MyDocument.getInitialProps = async ctx => {
     return {
         ...initialProps,
         // Styles fragment is rendered after the app and page rendering finish.
-        styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+        styles: [
+            ...React.Children.toArray(initialProps.styles),
+            sheets.getStyleElement(),
+        ],
     };
 };

712421ecdec4cfe81084c96a61503667389365a6
sriram srinivasan <sriram@cyverse.org>
2020-02-10 21:15:12 -0500

    Fix style breaks on refresh

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,12 @@ import theme from "../components/theme/default";
 import { ThemeProvider } from "@material-ui/core/styles";
 
 function MyApp({ Component, pageProps }) {
+    React.useEffect(() => {
+        const jssStyles = document.querySelector("#jss-server-side");
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
     return (
         <ThemeProvider theme={theme}>
             <CyverseAppBar>

29e69cb24d7d3ec19f33921fd5fc6d5b98a5d403
Sriram Srinivasan <sriram@cyverse.org>
2020-02-12 10:13:22 -0500

    Move routing default logic for / to _app

--- src/server/index.js
+++ src/server/index.js
@@ -84,11 +84,6 @@ app.prepare()
         logger.info("adding the api router to the express server");
         server.use("/api", apiRouter());
 
-        //map root to Dashboard
-        server.get("/", (req, res) => {
-            app.render(req, res, "/dashboard", undefined);
-        });
-
         logger.info(
             "adding the next.js fallthrough handler to the express server."
         );

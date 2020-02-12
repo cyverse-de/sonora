63a21b56d5ad73e76cb265f9efa21e391ed01797
John Wregglesworth <wregglej@iplantcollaborative.org>
2020-02-12 14:45:39 -0700

    Reorganize the imports

--- src/components/URLImportDialog/index.js
+++ src/components/URLImportDialog/index.js
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
+import { injectIntl } from "react-intl";
 
-import { makeStyles } from "@material-ui/core/styles";
-import { useTheme } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme, makeStyles } from "@material-ui/core/styles";
+import { Publish as PublishIcon, Close as CloseIcon } from "@material-ui/icons";
 
-import PublishIcon from "@material-ui/icons/Publish";
-import Button from "@material-ui/core/Button";
-import CloseIcon from "@material-ui/icons/Close";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import IconButton from "@material-ui/core/IconButton";
-import TextField from "@material-ui/core/TextField";
-
-import intlData from "./messages";
-import ids from "./ids";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    IconButton,
+    TextField,
+    useMediaQuery,
+} from "@material-ui/core";
 
 import {
     build as buildID,
@@ -23,7 +21,8 @@ import {
     withI18N,
 } from "@cyverse-de/ui-lib";
 
-import { injectIntl } from "react-intl";
+import intlData from "./messages";
+import ids from "./ids";
 
 const useStyles = makeStyles((theme) => ({
     root: {

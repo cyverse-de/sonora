903c42e8c4534acbc9b22a7925e8cb0144f4d10f
Sarah Roberts <sarahr@cyverse.org>
2020-02-19 14:17:15 -0700

    CORE-868: added some comments to contexts/userProfile.js

--- src/contexts/userProfile.js
+++ src/contexts/userProfile.js
@@ -2,6 +2,12 @@ import React from "react";
 
 const UserProfileContext = React.createContext();
 
+/**
+ * A hook that returns state for obtaining and updating the client-side copy
+ * of the user profile information. The user profile information should only
+ * be set in one React component. The information can be read from any React
+ * component.
+ */
 function useUserProfile() {
     const context = React.useContext(UserProfileContext);
     if (!context) {
@@ -12,6 +18,16 @@ function useUserProfile() {
     return context;
 }
 
+/**
+ * A React component that wraps its descendants in user profile context
+ * providers, allowing them to obtain information about the authenticated
+ * user. This hook does not actually retrieve the user profile information
+ * from the server. Exactly one of the descendant components should make a
+ * call to the `/api/profile` endpoint in order to obtain the user profile
+ * information and store it using the `setUserProfile` function.
+ *
+ * @param {object} props the React component properties.
+ */
 function UserProfileProvider(props) {
     const [userProfile, setUserProfile] = React.useState(null);
     const value = React.useMemo(() => [userProfile, setUserProfile], [

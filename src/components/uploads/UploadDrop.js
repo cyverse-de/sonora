2340640596ac430a07d55decad2f1438b2422560
John Wregglesworth <wregglej@iplantcollaborative.org>
2020-02-24 12:04:36 -0700

    Remove old startUpload()

--- src/components/uploads/UploadDrop.js
+++ src/components/uploads/UploadDrop.js
@@ -8,13 +8,7 @@
 
 import UUID from "uuid/v4";
 
-import {
-    addAction,
-    errorAction,
-    updateStatusAction,
-} from "../../contexts/uploadTracking";
-
-import { checkForError } from "../../common/callApi";
+import { addAction } from "../../contexts/uploadTracking";
 
 /**
  * Turns a DataTransferItemList into an actual array/list.
@@ -54,74 +48,6 @@ export const trackUpload = (uploadFile, destinationPath, dispatch) => {
             file: uploadFile,
         })
     );
-};
-
-/**
- * Starts a single upload.
- *
- * @param {File} uploadFile - The File object to be uploaded.
- * @param {string} destinationPath - The path to the directory the file should be uploaded to.
- * @return null
- */
-export const startUpload = (
-    uploadFile,
-    destinationPath,
-    dispatch,
-    completedCB = () => {}
-) => {
-    const newID = UUID();
-
-    const formData = new FormData();
-    formData.append("file", uploadFile);
-
-    dispatch(
-        addAction({
-            id: newID,
-            parentPath: destinationPath,
-            filename: uploadFile.name,
-            isUploading: true,
-            hasUploaded: false,
-        })
-    );
-
-    const endpoint = `/api/upload?dest=${destinationPath}`;
-    const method = "POST";
-
-    fetch(endpoint, {
-        method,
-        credentials: "include",
-        body: formData,
-    })
-        .then((resp) => checkForError(resp, { method, endpoint, headers: {} }))
-        .then((resp) => {
-            dispatch(
-                updateStatusAction({
-                    id: newID,
-                    hasUploaded: true,
-                    isUploading: false,
-                })
-            );
-
-            completedCB(newID);
-
-            return resp;
-        })
-        .catch((e) => {
-            if (e.details) {
-                console.error(
-                    `${e.details.code} ${JSON.stringify(e.details.context)}`
-                );
-            } else {
-                console.error(e.message);
-            }
-
-            dispatch(
-                errorAction({
-                    id: newID,
-                    errorMessage: e.message,
-                })
-            );
-        });
 };
 
 /**

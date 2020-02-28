8f76b75f34b476f0c6f53de0323974bdf77eed09
Sriram Srinivasan <sriram@cyverse.org>
2020-02-28 14:26:22 -0500

    Add snapshot test.

--- src/__tests__/data.js
+++ src/__tests__/data.js
@@ -2,9 +2,16 @@ import React from "react";
 import renderer from "react-test-renderer";
 
 import { DataTableViewTest } from "../../stories/data/TableView.stories";
+import { DataNavigationTest } from "../../stories/data/DataNavigation.stories";
 
 test("Data Table View renders", () => {
     const component = renderer.create(<DataTableViewTest />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test("Data Navigation View renders", () => {
+    const component = renderer.create(<DataNavigationTest />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });

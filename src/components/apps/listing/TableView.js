1ba44f797a8e8864d358f43fe7323ca87b6c52e2
sriram srinivasan <sriram@cyverse.org>
2020-03-30 18:21:24 +0530

    Add app type filter.

--- src/components/apps/listing/TableView.js
+++ src/components/apps/listing/TableView.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * @author sriram
  *
@@ -6,8 +5,6 @@
  *
  */
 
-=======
->>>>>>> a5e0f9d... Basic app listing using react-query
 import React from "react";
 import {
     build,
@@ -37,52 +34,26 @@ import messages from "../messages";
 
 function getTableColumns(deletable, enableMenu) {
     let tableColumns = [
-<<<<<<< HEAD
-        {
-            name: "",
-            numeric: false,
-            enableSorting: false,
-            key: AppFields.STATUS.key,
-            id: AppFields.STATUS.key,
-        },
-=======
         { name: "", numeric: false, enableSorting: false, key: "status" },
->>>>>>> a5e0f9d... Basic app listing using react-query
         {
             name: AppFields.NAME.fieldName,
             enableSorting: true,
             key: AppFields.NAME.key,
-<<<<<<< HEAD
-            id: AppFields.NAME.key,
-=======
->>>>>>> a5e0f9d... Basic app listing using react-query
         },
         {
             name: AppFields.INTEGRATOR.fieldName,
             enableSorting: true,
             key: AppFields.INTEGRATOR.key,
-<<<<<<< HEAD
-            id: AppFields.INTEGRATOR.key,
-=======
->>>>>>> a5e0f9d... Basic app listing using react-query
         },
         {
             name: AppFields.RATING.fieldName,
             enableSorting: true,
             key: AppFields.RATING.key,
-<<<<<<< HEAD
-            id: AppFields.RATING.key,
-=======
->>>>>>> a5e0f9d... Basic app listing using react-query
         },
         {
             name: AppFields.SYSTEM.fieldName,
             enableSorting: false,
             key: AppFields.SYSTEM.key,
-<<<<<<< HEAD
-            id: AppFields.SYSTEM.key,
-=======
->>>>>>> a5e0f9d... Basic app listing using react-query
             align: "right",
         },
     ];
@@ -123,11 +94,7 @@ function TableView(props) {
     } = props;
     const apps = listing?.apps;
     const columnData = getTableColumns(false, false);
-<<<<<<< HEAD
-    const tableId = build(baseId, ids.LISTING_TABLE);
-=======
     const tableId = build(baseId, ids.listingTable);
->>>>>>> a5e0f9d... Basic app listing using react-query
     return (
         <TableContainer
             component={Paper}
@@ -139,14 +106,10 @@ function TableView(props) {
                 <EnhancedTableHead
                     selectable={true}
                     numSelected={selected.length}
-                    rowsInPage={listing?.apps.length}
+                    rowsInPage={listing?.apps ? listing.apps.length : 0}
                     order={order}
                     orderBy={orderBy}
-<<<<<<< HEAD
-                    baseId={tableId}
-=======
                     baseId="appTableView"
->>>>>>> a5e0f9d... Basic app listing using react-query
                     columnData={columnData}
                     onRequestSort={handleRequestSort}
                     onSelectAllClick={handleSelectAllClick}
@@ -171,118 +134,103 @@ function TableView(props) {
                             />
                         )}
                         {apps &&
-                            apps.length > 0 &&
-                            apps.map((app, index) => {
-                                const {
-                                    average: averageRating,
-                                    user: userRating,
-                                    total: totalRating,
-                                } = app.rating;
-                                const appId = app.id;
-                                const appName = app.name;
-                                const isSelected =
-                                    selected.indexOf(appId) !== -1;
-                                const rowId = build(baseId, appId);
-                                return (
-                                    <TableRow
-                                        role="checkbox"
-                                        tabIndex={-1}
-                                        hover
-                                        selected={isSelected}
-                                        aria-checked={isSelected}
-                                        key={app.id}
-                                        id={rowId}
-                                        onClick={(event) =>
-                                            handleClick(event, appId, index)
-                                        }
+                        apps.length > 0 &&
+                        apps.map((app, index) => {
+                            const {
+                                average: averageRating,
+                                user: userRating,
+                                total: totalRating,
+                            } = app.rating;
+                            const appId = app.id;
+                            const appName = app.name;
+                            const isSelected =
+                                selected.indexOf(appId) !== -1;
+                            const rowId = build(baseId, appId);
+                            return (
+                                <TableRow
+                                    role="checkbox"
+                                    tabIndex={-1}
+                                    hover
+                                    selected={isSelected}
+                                    aria-checked={isSelected}
+                                    key={app.id}
+                                    id={rowId}
+                                    onClick={(event) =>
+                                        handleClick(event, appId, index)
+                                    }
+                                >
+                                    <TableCell padding="checkbox">
+                                        <DECheckbox
+                                            checked={isSelected}
+                                            tabIndex={0}
+                                            id={build(
+                                                tableId,
+                                                appName,
+                                                ids.checkbox
+                                            )}
+                                            inputProps={{
+                                                "aria-label": formatMessage(
+                                                    intl,
+                                                    "ariaCheckbox",
+                                                    {
+                                                        label: appName,
+                                                    }
+                                                ),
+                                            }}
+                                        />
+                                    </TableCell>
+                                    <TableCell
+                                        padding="none"
+                                        id={build(
+                                            rowId,
+                                            ids.APP_STATUS_ICON
+                                        )}
                                     >
-                                        <TableCell padding="checkbox">
-                                            <DECheckbox
-                                                checked={isSelected}
-                                                tabIndex={0}
-                                                id={build(
-                                                    tableId,
-                                                    appName,
-                                                    ids.checkbox
-                                                )}
-                                                inputProps={{
-                                                    "aria-label": formatMessage(
-                                                        intl,
-                                                        "ariaCheckbox",
-                                                        {
-                                                            label: appName,
-                                                        }
-                                                    ),
-                                                }}
-                                            />
-                                        </TableCell>
-<<<<<<< HEAD
-                                        <TableCell
-                                            padding="none"
-                                            id={build(
+                                        <AppStatusIcon
+                                            isPublic={app.is_public}
+                                            isBeta={app.beta}
+                                            isDisabled={app.disabled}
+                                        />
+                                    </TableCell>
+                                    <TableCell>
+                                        <AppName
+                                            baseDebugId={build(
                                                 rowId,
-                                                ids.APP_STATUS_ICON
+                                                ids.APP_NAME
                                             )}
-                                        >
-=======
-                                        <TableCell padding="none">
->>>>>>> a5e0f9d... Basic app listing using react-query
-                                            <AppStatusIcon
-                                                isPublic={app.is_public}
-                                                isBeta={app.beta}
-                                                isDisabled={app.disabled}
-                                            />
-                                        </TableCell>
-                                        <TableCell>
-                                            <AppName
-                                                baseDebugId={build(
-                                                    rowId,
-                                                    ids.APP_NAME
-                                                )}
-                                                isDisabled={app.disabled}
-                                                name={app.name}
-                                            />
-                                        </TableCell>
-<<<<<<< HEAD
-                                        <TableCell
-                                            id={build(
-                                                rowId,
-                                                ids.integratorName
-                                            )}
-                                        >
-                                            {app.integrator_name}
-                                        </TableCell>
-                                        <TableCell
-                                            id={build(rowId, ids.RATING)}
-                                        >
-=======
-                                        <TableCell>
-                                            {app.integrator_name}
-                                        </TableCell>
-                                        <TableCell>
->>>>>>> a5e0f9d... Basic app listing using react-query
-                                            <Rate
-                                                name={app.id}
-                                                value={
-                                                    userRating || averageRating
-                                                }
-                                                readOnly={true}
-                                                total={totalRating}
-                                            />
-                                        </TableCell>
-<<<<<<< HEAD
-                                        <TableCell
-                                            align="right"
-                                            id={build(rowId, ids.SYSTEM_ID)}
-                                        >
-=======
-                                        <TableCell align="right">
->>>>>>> a5e0f9d... Basic app listing using react-query
-                                            {app.system_id}
-                                        </TableCell>
-                                    </TableRow>
-                                );
-                            })}
+                                            isDisabled={app.disabled}
+                                            name={app.name}
+                                        />
+                                    </TableCell>
+                                    <TableCell
+                                        id={build(
+                                            rowId,
+                                            ids.integratorName
+                                        )}
+                                    >
+                                        {app.integrator_name}
+                                    </TableCell>
+                                    <TableCell
+                                        id={build(rowId, ids.RATING)}
+                                    >
+                                        <Rate
+                                            name={app.id}
+                                            value={
+                                                userRating || averageRating
+                                            }
+                                            readOnly={true}
+                                            total={totalRating}
+                                        />
+                                    </TableCell>
+                                    <TableCell
+                                        align="right"
+                                        id={build(rowId, ids.SYSTEM_ID)}
+                                    >
+                                        {app.system_id}
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
                     </TableBody>
                 )}
             </Table>

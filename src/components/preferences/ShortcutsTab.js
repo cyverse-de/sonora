a03f3665c991b8a242c69298af13d6403f364470
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2020-04-01 15:52:23 -0700

    add text fields to formik + update to Typography

--- src/components/preferences/ShortcutsTab.js
+++ src/components/preferences/ShortcutsTab.js
@@ -3,10 +3,31 @@ import styles from "./styles";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
+import { Field } from "formik";
+
 const useStyles = makeStyles(styles);
 
 export default function ShortcutsTab() {
     const classes = useStyles();
+
+    const onTextChange = (setFieldValue, fieldName) => (event, value) => {
+        const new_value = event.target.value;
+        setFieldValue(fieldName, new_value);
+    };
+
+    const FormTextField = ({
+        field: { value, onChange, ...field },
+        form: { setFieldValue },
+        ...custom
+    }) => (
+        <TextField
+            value={value}
+            onChange={onTextChange(setFieldValue, field.name)}
+            size={"small"}
+            inputProps={{ maxLength: 1 }}
+            {...custom}
+        />
+    );
 
     return (
         <div>
@@ -14,64 +35,57 @@ export default function ShortcutsTab() {
                 Keyboard Shortcuts
             </Typography>
             <br />
-            Open apps view
-            <div className={classes.shortcut}>
+            <Typography className={classes.section} display="inline">
+                Open apps view
+            </Typography>
+            <Typography display="inline" className={classes.shortcut}>
                 Control + Shift +
-                <TextField
-                    defaultValue={"A"}
+                <Field
+                    component={FormTextField}
+                    name="preferences.appsKBShortcut"
                     className={classes.shortcutField}
-                    size={"small"}
-                    inputProps={{ maxLength: 1 }}
                 />
-            </div>
+            </Typography>
             <br />
             <br />
-            Open data view
-            <div className={classes.shortcut}>
+            <Typography className={classes.section} display="inline">
+                Open data view
+            </Typography>
+            <Typography display="inline" className={classes.shortcut}>
                 Control + Shift +
-                <TextField
-                    defaultValue={"A"}
+                <Field
+                    component={FormTextField}
+                    name="preferences.dataKBShortcut"
                     className={classes.shortcutField}
-                    size={"small"}
-                    inputProps={{ maxLength: 1 }}
                 />
-            </div>
+            </Typography>
             <br />
             <br />
-            Open analyses view
-            <div className={classes.shortcut}>
+            <Typography className={classes.section} display="inline">
+                Open analyses view
+            </Typography>
+            <Typography display="inline" className={classes.shortcut}>
                 Control + Shift +
-                <TextField
-                    defaultValue={"A"}
+                <Field
+                    component={FormTextField}
+                    name="preferences.analysisKBShortcut"
                     className={classes.shortcutField}
-                    size={"small"}
-                    inputProps={{ maxLength: 1 }}
                 />
-            </div>
+            </Typography>
             <br />
             <br />
-            Open notifications view
-            <div className={classes.shortcut}>
+            <Typography className={classes.section} display="inline">
+                Open notifications view
+            </Typography>
+            <Typography display="inline" className={classes.shortcut}>
                 Control + Shift +
-                <TextField
-                    defaultValue={"A"}
+                <Field
+                    component={FormTextField}
+                    name="preferences.notificationKBShortcut"
                     className={classes.shortcutField}
-                    size={"small"}
-                    inputProps={{ maxLength: 1 }}
                 />
-            </div>
+            </Typography>
             <br />
-            <br />
-            Open active view
-            <div className={classes.shortcut}>
-                Control + Shift +
-                <TextField
-                    defaultValue={"A"}
-                    className={classes.shortcutField}
-                    size={"small"}
-                    inputProps={{ maxLength: 1 }}
-                />
-            </div>
         </div>
     );
 }

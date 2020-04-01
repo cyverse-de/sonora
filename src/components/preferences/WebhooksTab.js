8d8af485e277e0239a26cd61d33baa0608365bbe
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2020-04-01 16:03:58 -0700

    WIP

--- src/components/preferences/WebhooksTab.js
+++ src/components/preferences/WebhooksTab.js
@@ -3,49 +3,92 @@ import styles from "./styles";
 import { makeStyles } from "@material-ui/core/styles";
 import Switch from "@material-ui/core/Switch";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { Field } from "formik";
+import MenuItem from "@material-ui/core/MenuItem";
+import {
+    FormSelectField,
+    FormTextField,
+} from "@cyverse-de/ui-lib/dist/util/FormField";
+
 const useStyles = makeStyles(styles);
 
-export default function WebhooksTab(props) {
+export default function WebhooksTab() {
     const classes = useStyles();
-
-    const onSwitchChange = (setFieldValue, fieldName) => (event, checked) => {
-        setFieldValue(fieldName, checked);
+    const webhookTypes = ["Slack", "Zapier", "Custom"];
+    const onSwitchChange = (setFieldValue, fieldName, values, tag) => (
+        event,
+        checked
+    ) => {
+        let aux = [...values.apps_info.webhooks[0].topics];
+        if (values.apps_info.webhooks[0].topics.includes(tag) === false) {
+            aux.push(tag);
+        } else if (values.apps_info.webhooks[0].topics.includes(tag) === true) {
+            aux = aux.filter((e) => e !== tag);
+        }
+        console.log(fieldName);
+        setFieldValue(fieldName, aux);
     };
 
     const FormSwitch = ({
-        field: { value, onChange, ...field },
-        form: { setFieldValue },
+        field: { value, onChange, checked, ...field },
+        form: { setFieldValue, values },
+        tag,
         ...custom
     }) => (
         <Switch
-            checked={!!value}
-            onChange={onSwitchChange(setFieldValue, field.name)}
+            onChange={onSwitchChange(setFieldValue, field.name, values, tag)}
+            checked={values.apps_info.webhooks[0].topics.includes(tag)}
             {...custom}
         />
+    );
+
+    const deleteWebhook = (setFieldValue, values) => (event) => {
+        console.log(values.apps_info.webhooks);
+        values.apps_info.webhooks.pop();
+        console.log(values.apps_info.webhooks);
+        setFieldValue("apps_info.webhooks", values.apps_info.webhooks);
+    };
+
+    const WebhookDeleteButton = ({
+        form: { setFieldValue, values },
+        ...custom
+    }) => (
+        <IconButton onClick={deleteWebhook(setFieldValue, values)} {...custom}>
+            <DeleteIcon fontSize="large" />
+        </IconButton>
     );
 
     return (
         <div>
             <Typography className={classes.sectionHeader}>Webhooks</Typography>
             <br />
-            <TextField
-                className={classes.textField}
+            <Field
+                component={FormTextField}
+                className={classes.webhooksTextField}
                 label="Path"
                 defaultValue="https://"
                 variant="outlined"
+                name="apps_info.webhooks[0].url"
             />
-            <TextField
-                label="Name"
-                variant="outlined"
-                className={classes.smallTextField}
+            <Field
+                component={FormSelectField}
+                name="apps_info.webhooks[0].type.type"
+                className={classes.dropDown}
+                fullWidth={false}
+            >
+                {webhookTypes.map((type, index) => (
+                    <MenuItem key={index} value={type}>
+                        {type}
+                    </MenuItem>
+                ))}
+            </Field>
+            <Field
+                aria-label="delete"
+                color="inherit"
+                component={WebhookDeleteButton}
             />
-            <IconButton aria-label="delete" color="inherit">
-                <DeleteIcon fontSize="large" />
-            </IconButton>
             <br />
             <br />
             <Typography className={classes.sectionHeader}>
@@ -55,7 +98,8 @@ export default function WebhooksTab(props) {
             Data (e.g: When someone shares a file with you.)
             <Field
                 component={FormSwitch}
-                value="enableData"
+                tag="data"
+                name="apps_info.webhooks[0].topics"
                 color="primary"
                 inputProps={{ "aria-label": "Data" }}
                 className={classes.toggle}
@@ -65,7 +109,8 @@ export default function WebhooksTab(props) {
             Apps (e.g: When someone shares an app with you.)
             <Field
                 component={FormSwitch}
-                value="enableApps"
+                tag="apps"
+                name="apps_info.webhooks[0].topics"
                 color="primary"
                 inputProps={{ "aria-label": "Apps" }}
                 className={classes.toggle}
@@ -75,7 +120,8 @@ export default function WebhooksTab(props) {
             Analyses (e.g: When your analysis status changes.)
             <Field
                 component={FormSwitch}
-                value="enableAnalyses"
+                tag="analysis"
+                name="apps_info.webhooks[0].topics"
                 color="primary"
                 inputProps={{ "aria-label": "Analyses" }}
                 className={classes.toggle}
@@ -85,12 +131,24 @@ export default function WebhooksTab(props) {
             Tools (e.g: When your tool request is complete.)
             <Field
                 component={FormSwitch}
-                value="enableTools"
+                tag="tool_request"
+                name="apps_info.webhooks[0].topics"
                 color="primary"
                 inputProps={{ "aria-label": "Tools" }}
                 className={classes.toggle}
             />
             <br />
+            <br />
+            Teams (e.g: When someone sends you a team invite or joins your
+            team.)
+            <Field
+                component={FormSwitch}
+                tag="team"
+                name="apps_info.webhooks[0].topics"
+                color="primary"
+                inputProps={{ "aria-label": "Team" }}
+                className={classes.toggle}
+            />
         </div>
     );
 }

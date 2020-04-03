0df9d8f3ff7905cd60d9f2c6159f65b826f8baf8
sriram srinivasan <sriram@cyverse.org>
2020-04-04 00:08:19 +0530

    Fix error handling. Allow react-query to handle errors.

--- src/common/callApi.js
+++ src/common/callApi.js
@@ -41,8 +41,9 @@ const callApi = (props) => {
         body,
         headers,
         setLoading,
-        setError,
+        setError
     } = props;
+
 
     const requestOptions = {
         headers: headers

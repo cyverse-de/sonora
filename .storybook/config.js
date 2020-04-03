eae5f5794ad258379bf2c1582050612b870878fd
Ash <asherz@email.arizona.edu>
2020-04-03 16:47:11 -0700

    Update storybook config to include UserProfileProvider

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,9 +3,14 @@ import { withConsole } from "@storybook/addon-console";
 import { addDecorator, configure } from "@storybook/react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../src/components/theme/default";
+import { UserProfileProvider } from "../src/contexts/userProfile";
 
 addDecorator((storyFn) => (
-    <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+    <ThemeProvider theme={theme}>
+        <UserProfileProvider>
+                {storyFn()}
+        </UserProfileProvider>
+    </ThemeProvider>
 ));
 
 //redirect console error / logs / warns to action logger

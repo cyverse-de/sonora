9c4a74eb84f4ac5f985afaa090018b961110c9c5
Ash <asherz@email.arizona.edu>
2020-04-03 17:08:55 -0700

    Add mock user profile to storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { withConsole } from "@storybook/addon-console";
 import { addDecorator, configure } from "@storybook/react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../src/components/theme/default";
-import { UserProfileProvider } from "../src/contexts/userProfile";
+import {
+    UserProfileProvider,
+    useUserProfile,
+} from "../src/contexts/userProfile";
+
+function MockUserProfile() {
+    const [userProfile, setUserProfile] = useUserProfile();
+    useEffect(() => {
+        if (!userProfile) {
+            setUserProfile({
+                id: "ipcdev",
+                attributes: {
+                    email: "ipcdev@cyverse.org",
+                    entitlement: ["some-entitlement"],
+                    firstName: "Iplant",
+                    lastName: "Dev",
+                    name: "Iplant Dev",
+                },
+            });
+        }
+    }, [setUserProfile]);
+
+    return <div />;
+}
 
 addDecorator((storyFn) => (
     <ThemeProvider theme={theme}>
         <UserProfileProvider>
-                {storyFn()}
+            <MockUserProfile />
+            {storyFn()}
         </UserProfileProvider>
     </ThemeProvider>
 ));

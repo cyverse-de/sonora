2a2b1fbc4d9232544fb4c0b001bb9feaace4ad23
Paul Sarando <psarando@iplantcollaborative.org>
2020-04-17 16:50:20 -0700

    CORE-934 Replace Reference Genome field loading spinner with skeleton.

--- src/components/apps/launch/ReferenceGenomeSelect.js
+++ src/components/apps/launch/ReferenceGenomeSelect.js
@@ -1,17 +1,75 @@
+/**
+ * @author psarando
+ *
+ * Form fields for selecting Reference Genome/Sequence/Annotation parameters.
+ * Can also display as field loading skeletons while selection lists are fetched
+ * from the backed services.
+ */
 import React from "react";
+
+import ids from "../../utils/ids";
 
 import messages from "./messages";
 
-import { FormTextField, getMessage, withI18N } from "@cyverse-de/ui-lib";
+import {
+    build as buildDebugId,
+    FormTextField,
+    getMessage,
+    getFormError,
+    withI18N,
+} from "@cyverse-de/ui-lib";
 
-import { CircularProgress, MenuItem } from "@material-ui/core";
+import {
+    FormControl,
+    InputLabel,
+    FormHelperText,
+    MenuItem,
+    makeStyles,
+} from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
+
+const useStyles = makeStyles((theme) => ({
+    skeleton: {
+        height: theme.spacing(6),
+    },
+}));
+
+const FormFieldLoading = ({
+    id,
+    field: { name },
+    label,
+    required,
+    form: { touched, errors },
+    ...props
+}) => {
+    const classes = useStyles();
+    const errorMsg = getFormError(name, touched, errors);
+    const loadingFieldID = buildDebugId(id, ids.LOADING_SKELETON);
+    const helperTextID = buildDebugId(loadingFieldID, "helper-text");
+
+    return (
+        <FormControl fullWidth error={!!errorMsg}>
+            <InputLabel htmlFor={loadingFieldID} required={required}>
+                {label}
+            </InputLabel>
+            <Skeleton
+                id={loadingFieldID}
+                aria-describedby={helperTextID}
+                variant="text"
+                className={classes.skeleton}
+                {...props}
+            />
+            <FormHelperText id={helperTextID}>{errorMsg}</FormHelperText>
+        </FormControl>
+    );
+};
 
 const ReferenceGenomeSelect = withI18N(
     ({ referenceGenomes, referenceGenomesLoading, ...props }) => {
         const selectProps = { ...props };
 
         if (referenceGenomesLoading) {
-            selectProps.SelectProps = { IconComponent: CircularProgress };
+            return <FormFieldLoading {...selectProps} />;
         } else if (!referenceGenomes?.length) {
             selectProps.error = true;
             selectProps.helperText = getMessage("errorLoadingReferenceGenomes");

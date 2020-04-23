5f0c88fe1b7840b1984d9b13aa70d56cdcfcb04c
John Wregglesworth <wregglej@cyverse.org>
2020-04-23 07:54:38 -0700

    Add initial vice admin page and story

---
+++ stories/vice/admin/VICEAdmin.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import VICEAdmin from "../../../src/components/vice/admin";
+
+export default {
+    title: "VICE/admin/VICEAdmin",
+};
+
+export const VICEAdminTest = () => {
+    return <VICEAdmin />;
+};

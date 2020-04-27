7eea348794070c0f1bbfc8f2695bae67c7dc0326
John Wregglesworth <wregglej@cyverse.org>
2020-04-27 14:53:48 -0700

    Begin adding the actual API calls to the admin UI

--- src/components/vice/admin/index.js
+++ src/components/vice/admin/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { useQuery } from "react-query";
+
 import { makeStyles } from "@material-ui/styles";
 
 import {
@@ -8,11 +10,14 @@ import {
     withI18N,
 } from "@cyverse-de/ui-lib";
 
+import getData from "../../../serviceFacades/vice/admin";
+
 import AnalysesFilter from "./filter";
 import AnalysisTable from "./analyses";
 
 import ids from "./ids";
 import messages from "./messages";
+import { Skeleton } from "@material-ui/lab";
 
 const id = (value) => buildID(ids.ROOT, value);
 
@@ -23,63 +28,95 @@ const useStyles = makeStyles((theme) => ({
         flexWrap: "wrap",
         justifyContent: "center",
     },
+    filterSkeleton: {
+        marginBottom: theme.spacing(5),
+    },
 }));
 
-const testData = {
-    deployments: [
-        {
-            group: 1000,
-            appName: "jupyter-lab-scipy-notebook-latest",
-            analysisName: "jupyter-lab-scipy-notebook-latest-analysis",
-            creationTimestamp: "2020-03-26 10:28:39 -0700 MST",
-            name: "0f809627-361a-4ab6-97ea-76bf7b6f19a3",
-            username: "ipctest",
-            command: [],
-            appID: "bc93504c-d584-11e9-8413-008cfa5ae621",
-            port: 8888,
-            externalID: "0f809627-361a-4ab6-97ea-76bf7b6f19a3",
-            userID: "843c85dc-3629-11ea-93db-008cfa5ae621",
-            image: "cyversevice/jupyterlab-scipy:latest",
-            namespace: "vice-apps",
-            user: 1000,
-        },
-        {
-            group: 1000,
-            appName: "copy-of-nanodj",
-            analysisName: "copy-of-nanodj-analysis",
-            creationTimestamp: "2020-03-26 10:20:29 -0700 MST",
-            name: "28655b9a-5959-4157-baf1-2436a83d19e7",
-            username: "ipcdev",
-            command: [],
-            appID: "57712906-d5e7-11e9-869a-008cfa5ae621",
-            port: 8888,
-            externalID: "28655b9a-5959-4157-baf1-2436a83d19e7",
-            userID: "6c04cd3e-854a-11e4-8fa0-1fbef07e6168",
-            image: "cyverse/jupyterlab-nanodj:latest",
-            namespace: "vice-apps",
-            user: 1000,
-        },
-        {
-            group: 1000,
-            appName: "jupyter-lab-scipy-google-earth-engine",
-            analysisName: "jupyter-lab-scipy-google-earth-engine-analysis",
-            creationTimestamp: "2020-01-31 18:35:21 -0700 MST",
-            name: "e112629a-67ec-4018-bf28-30b244e940c3",
-            username: "not-a-user",
-            command: [],
-            appID: "1f5e7f3a-e46c-11e9-870d-008cfa5ae621",
-            port: 8888,
-            externalID: "e112629a-67ec-4018-bf28-30b244e940c3",
-            userID: "6bec60d2-854a-11e4-b87e-1f417f9dbc81",
-            image: "cyversevice/jupyterlab-scipy:gee-latest",
-            namespace: "vice-apps",
-            user: 1000,
-        },
-    ],
+// const testData = {
+//     deployments: [
+//         {
+//             group: 1000,
+//             appName: "jupyter-lab-scipy-notebook-latest",
+//             analysisName: "jupyter-lab-scipy-notebook-latest-analysis",
+//             creationTimestamp: "2020-03-26 10:28:39 -0700 MST",
+//             name: "0f809627-361a-4ab6-97ea-76bf7b6f19a3",
+//             username: "ipctest",
+//             command: [],
+//             appID: "bc93504c-d584-11e9-8413-008cfa5ae621",
+//             port: 8888,
+//             externalID: "0f809627-361a-4ab6-97ea-76bf7b6f19a3",
+//             userID: "843c85dc-3629-11ea-93db-008cfa5ae621",
+//             image: "cyversevice/jupyterlab-scipy:latest",
+//             namespace: "vice-apps",
+//             user: 1000,
+//         },
+//         {
+//             group: 1000,
+//             appName: "copy-of-nanodj",
+//             analysisName: "copy-of-nanodj-analysis",
+//             creationTimestamp: "2020-03-26 10:20:29 -0700 MST",
+//             name: "28655b9a-5959-4157-baf1-2436a83d19e7",
+//             username: "ipcdev",
+//             command: [],
+//             appID: "57712906-d5e7-11e9-869a-008cfa5ae621",
+//             port: 8888,
+//             externalID: "28655b9a-5959-4157-baf1-2436a83d19e7",
+//             userID: "6c04cd3e-854a-11e4-8fa0-1fbef07e6168",
+//             image: "cyverse/jupyterlab-nanodj:latest",
+//             namespace: "vice-apps",
+//             user: 1000,
+//         },
+//         {
+//             group: 1000,
+//             appName: "jupyter-lab-scipy-google-earth-engine",
+//             analysisName: "jupyter-lab-scipy-google-earth-engine-analysis",
+//             creationTimestamp: "2020-01-31 18:35:21 -0700 MST",
+//             name: "e112629a-67ec-4018-bf28-30b244e940c3",
+//             username: "not-a-user",
+//             command: [],
+//             appID: "1f5e7f3a-e46c-11e9-870d-008cfa5ae621",
+//             port: 8888,
+//             externalID: "e112629a-67ec-4018-bf28-30b244e940c3",
+//             userID: "6bec60d2-854a-11e4-b87e-1f417f9dbc81",
+//             image: "cyversevice/jupyterlab-scipy:gee-latest",
+//             namespace: "vice-apps",
+//             user: 1000,
+//         },
+//     ],
+// };
+
+const VICEAdminSkeleton = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <Skeleton
+                variant="rect"
+                animation="wave"
+                height={100}
+                width="100%"
+                classes={{ root: classes.filterSkeleton }}
+            />
+            <Skeleton
+                variant="rect"
+                animation="wave"
+                height={300}
+                width="100%"
+            />
+        </>
+    );
 };
 
 const VICEAdmin = () => {
     const classes = useStyles();
+
+    const { status, data, error } = useQuery("vice-admin", getData);
+    const isLoading = status === "loading";
+    const hasErrored = status === "error";
+
+    if (hasErrored) {
+        console.log(error.message);
+    }
 
     const [filters, setFilters] = useState({});
 
@@ -93,12 +130,18 @@ const VICEAdmin = () => {
 
     return (
         <div id={id(ids.ROOT)} className={classes.root}>
-            <AnalysesFilter
-                filters={filters}
-                addToFilters={addToFilters}
-                deleteFromFilters={deleteFromFilters}
-            />
-            <AnalysisTable data={testData} />
+            {isLoading ? (
+                <VICEAdminSkeleton />
+            ) : (
+                <>
+                    <AnalysesFilter
+                        filters={filters}
+                        addToFilters={addToFilters}
+                        deleteFromFilters={deleteFromFilters}
+                    />
+                    <AnalysisTable data={data} />
+                </>
+            )}
         </div>
     );
 };

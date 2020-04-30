c4005eed07489b436866ab7f0865705788159cbe
Sarah Roberts <sarahr@cyverse.org>
2020-04-30 16:35:55 -0700

    CORE-994: added the API endpoint for listing tools

--- src/server/api/router.js
+++ src/server/api/router.js
@@ -297,5 +297,15 @@ export default function apiRouter() {
         })
     );
 
+    logger.info("adding the GET /tools handler");
+    api.get(
+        "/tools",
+        auth.authnTokenMiddleware,
+        terrainHandler({
+            method: "GET",
+            pathname: "/tools",
+        })
+    );
+
     return api;
 }

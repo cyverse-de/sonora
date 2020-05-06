22ce22f7802fc84882c2a17bdd1f2266c830945b
John Wregglesworth <wregglej@cyverse.org>
2020-05-06 09:20:34 -0700

    Refactor FieldSelect and FilterSection to cut down on code

--- src/components/vice/admin/filter/selects.js
+++ src/components/vice/admin/filter/selects.js
@@ -5,10 +5,8 @@ import { Select, InputLabel, MenuItem } from "@material-ui/core";
 import { getMessage as msg } from "@cyverse-de/ui-lib";
 
 import useStyles from "./styles";
-import { id } from "./functions";
-import ids from "./ids";
 
-const FieldSelect = ({
+export const FieldSelect = ({
     id,
     kind,
     labelI18nKey = "filterField",
@@ -43,124 +41,48 @@ const defaultFields = {
     namespace: "$.analyses.namespace",
 };
 
-export const DeploymentFieldSelect = ({ value, handleChange }) => {
-    const fields = {
-        ...defaultFields,
-        image: "$.deployments.image",
-        port: "$.deployments.port",
-        uid: "$.deployments.uid",
-        gid: "$.deployments.gid",
-    };
-
-    const idValue = id(ids.DEPLOYMENT_FIELD_SELECT);
-    return (
-        <FieldSelect
-            id={idValue}
-            kind="deployment"
-            fields={fields}
-            value={value}
-            handleChange={handleChange}
-        />
-    );
+export const deploymentFields = {
+    ...defaultFields,
+    image: "$.deployments.image",
+    port: "$.deployments.port",
+    uid: "$.deployments.uid",
+    gid: "$.deployments.gid",
 };
 
-export const ServiceFieldSelect = ({ value, handleChange }) => {
-    const fields = {
-        ...defaultFields,
-        portName: "$.services.portName",
-        nodePort: "$.services.nodePort",
-        targetPort: "$.services.targetPort",
-        targetPortName: "$.services.targetPortName",
-        port: "$.services.port",
-        protocol: "$.services.protocol",
-    };
-
-    const idValue = id(ids.SERVICE_FIELD_SELECT);
-
-    return (
-        <FieldSelect
-            id={idValue}
-            fields={fields}
-            value={value}
-            handleChange={handleChange}
-        />
-    );
+export const serviceFields = {
+    ...defaultFields,
+    portName: "$.services.portName",
+    nodePort: "$.services.nodePort",
+    targetPort: "$.services.targetPort",
+    targetPortName: "$.services.targetPortName",
+    port: "$.services.port",
+    protocol: "$.services.protocol",
 };
 
-export const ConfigMapFieldSelect = ({ value, handleChange }) => {
-    const fields = defaultFields;
+export const configMapFields = defaultFields;
 
-    const idValue = id(ids.CONFIGMAP_FIELD_SELECT);
+export const ingressFields = defaultFields;
 
-    return (
-        <FieldSelect
-            id={idValue}
-            fields={fields}
-            value={value}
-            handleChange={handleChange}
-        />
-    );
+export const analysisFields = {
+    ...defaultFields,
+    analysisName: "$.analyses.analysisName",
+    appName: "$.analyses.appName",
+    appID: "$.analyses.appID",
+    externalID: "$.analyses.externalID",
+    userID: "$.analyses.userID",
+    username: "$.analyses.username",
+    dateCreated: "$.analyses.creationTimestamp",
 };
 
-export const IngressFieldSelect = ({ value, handleChange }) => {
-    const fields = defaultFields;
-    const idValue = id(ids.INGRESS_FIELD_SELECT);
-
-    return (
-        <FieldSelect
-            id={idValue}
-            fields={fields}
-            value={value}
-            handleChange={handleChange}
-        />
-    );
-};
-
-export const AnalysisFieldSelect = ({ value, handleChange }) => {
-    const fields = {
-        ...defaultFields,
-        analysisName: "$.analyses.analysisName",
-        appName: "$.analyses.appName",
-        appID: "$.analyses.appID",
-        externalID: "$.analyses.externalID",
-        userID: "$.analyses.userID",
-        username: "$.analyses.username",
-        dateCreated: "$.analyses.creationTimestamp",
-    };
-    const idValue = id(ids.ANALYSIS_FIELD_SELECT);
-
-    return (
-        <FieldSelect
-            id={idValue}
-            fields={fields}
-            value={value}
-            handleChange={handleChange}
-        />
-    );
-};
-
-export const PodFieldSelect = ({ value, handleChange }) => {
-    const fields = {
-        ...defaultFields,
-        phase: "$.pods.phase",
-        message: "$.pods.message",
-        reason: "$.pods.reason",
-        containerStatusName: "$.pods.containerStatusName",
-        containerStatusReady: "$.pods.containerStatusReady",
-        containerStatusRestartCount: "$.pods.containerStatusRestartCount",
-        containerStatusImage: "$.pods.containerStatusImage",
-        containerStatusImageID: "$.pods.containerStatusImageID",
-        containerStatusContainerID: "$.pods.containerStatusContainerID",
-        containerStatusStarted: "$.pods.containerStatusStarted",
-    };
-    const idValue = id(ids.POD_FIELD_SELECT);
-
-    return (
-        <FieldSelect
-            id={idValue}
-            fields={fields}
-            value={value}
-            handleChange={handleChange}
-        />
-    );
+export const podFields = {
+    phase: "$.pods.phase",
+    message: "$.pods.message",
+    reason: "$.pods.reason",
+    containerStatusName: "$.pods.containerStatusName",
+    containerStatusReady: "$.pods.containerStatusReady",
+    containerStatusRestartCount: "$.pods.containerStatusRestartCount",
+    containerStatusImage: "$.pods.containerStatusImage",
+    containerStatusImageID: "$.pods.containerStatusImageID",
+    containerStatusContainerID: "$.pods.containerStatusContainerID",
+    containerStatusStarted: "$.pods.containerStatusStarted",
 };

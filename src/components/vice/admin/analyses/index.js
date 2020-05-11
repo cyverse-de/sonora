5a3eeeca0275159c9422f6092adab2c1cbb7b338
John Wregglesworth <wregglej@cyverse.org>
2020-05-11 09:01:32 -0700

    Convert to collapsible rows for the analyses table

--- src/components/vice/admin/analyses/index.js
+++ src/components/vice/admin/analyses/index.js
@@ -9,21 +9,26 @@ import {
 
 import {
     //makeStyles,
+    Box,
+    Collapse,
     Table,
     TableBody,
     TableCell,
     TableContainer,
+    TableHead,
     Paper,
     TableRow,
     makeStyles,
+    IconButton,
 } from "@material-ui/core";
 
 import messages from "./messages";
 import ids from "./ids";
 import { COLUMNS } from "./constants";
+import { KeyboardArrowUp, KeyboardArrowDown } from "@material-ui/icons";
 
 // Constructs an ID for an element.
-const id = (name) => buildID(ids.BASE, name);
+const id = (...names) => buildID(ids.BASE, ...names);
 
 const defineColumn = (
     name,
@@ -44,10 +49,16 @@ const useStyles = makeStyles((theme) => ({
     table: {
         height: "100%",
     },
+    row: {
+        "& > *": {
+            borderBottom: "unset",
+        },
+    },
 }));
 
 // The column definitions for the table.
 const tableColumns = [
+    defineColumn("", COLUMNS.EXPAND, "", "left", false),
     defineColumn("Username", COLUMNS.USERNAME, "username"),
     defineColumn("Analysis Name", COLUMNS.ANALYSIS_NAME, "analysisName"),
     defineColumn("App Name", COLUMNS.APP_NAME, "appName"),
@@ -71,11 +82,81 @@ const getAnalyses = ({ deployments }) => {
                 analysisName: element.analysisName,
                 appName: element.appName,
                 creationTimestamp: element.creationTimestamp,
+                appID: element.appID,
+                userID: element.userID,
+                namespace: element.namespace,
             };
         }
     });
 
     return Object.values(analyses);
+};
+
+const AnalysisRow = ({ row }) => {
+    const [open, setOpen] = useState(false);
+    const classes = useStyles();
+
+    return (
+        <>
+            <TableRow
+                className={classes.row}
+                key={row.externalID}
+                id={id(row.externalID)}
+            >
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+                    </IconButton>
+                </TableCell>
+
+                {tableColumns.slice(1).map((column) => (
+                    <TableCell key={id(row.externalID, column.field)}>
+                        {row[column.field]}
+                    </TableCell>
+                ))}
+            </TableRow>
+
+            <TableRow
+                key={`${row.externalID}.collapse`}
+                id={id(row.externalID, "collapse")}
+            >
+                <TableCell
+                    style={{ paddingBottom: 0, paddingTop: 0 }}
+                    colSpan={6}
+                >
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box margin={1}>
+                            <Table
+                                size="small"
+                                aria-label="more analyses fields"
+                            >
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>ExternalID</TableCell>
+                                        <TableCell>AppID</TableCell>
+                                        <TableCell>UserID</TableCell>
+                                        <TableCell>Namespace</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>{row.externalID}</TableCell>
+                                        <TableCell>{row.appID}</TableCell>
+                                        <TableCell>{row.userID}</TableCell>
+                                        <TableCell>{row.namespace}</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </>
+    );
 };
 
 const AnalysisTable = ({ data }) => {
@@ -105,17 +186,7 @@ const AnalysisTable = ({ data }) => {
                 ></EnhancedTableHead>
                 <TableBody>
                     {analyses.map((row) => (
-                        <TableRow key={row.externalID} id={id(row.externalID)}>
-                            {tableColumns.map((column) => (
-                                <TableCell
-                                    key={id(
-                                        `${row.externalID}.${column.field}`
-                                    )}
-                                >
-                                    {row[column.field]}
-                                </TableCell>
-                            ))}
-                        </TableRow>
+                        <AnalysisRow row={row} key={row.externalID} />
                     ))}
                 </TableBody>
             </Table>

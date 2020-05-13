898cf40d6cb1e4b99cea33f5cad3b19cff8a2cef
Paul Sarando <psarando@iplantcollaborative.org>
2020-05-12 19:00:35 -0700

    CORE-731 Rename Pipeline story and filename.
    Fix typo.

--- stories/apps/launch/Pipeline.stories.js
+++ stories/apps/launch/Pipeline.stories.js
@@ -3,6 +3,6 @@ import React from "react";
 import AppLaunchStoryBase from "./AppLaunchStoryBase";
 import PipelineApp from "./data/PipelineApp";
 
-export const Pipline = () => <AppLaunchStoryBase app={PipelineApp} />;
+export const Pipeline = () => <AppLaunchStoryBase app={PipelineApp} />;
 
 export default { title: "Apps / Launch" };

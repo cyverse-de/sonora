8c17000ecf186ca844410bb3cae0dd2b2a184030
sriram srinivasan <sriram@cyverse.org>
2020-05-13 12:34:41 +0530

    remove storybook-static folder that was created with chromatic.

--- storybook-static/main.b0fd68ddc52a208de923.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1423:function(n,o,c){"use strict";c.r(o);c(1424),c(1483)},552:function(n,o,c){c(553),c(696),n.exports=c(1423)},615:function(n,o){}},[[552,1,2]]]);

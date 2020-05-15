06d64adc725259853731cf28a18af797801eebcb
Ian McEwen <mian@cyverse.org>
2020-05-15 16:03:03 -0700

    Add @next/bundle-analyzer in a non-ad-hoc way

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const config = require("config");
 
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
     env: {
         INTERCOM_APP_ID: config.get("intercom.app_id"),
         INTERCOM_ENABLED: config.get("intercom.enabled"),
         INTERCOM_COMPANY_ID: config.get("intercom.company_id"),
         INTERCOM_COMPANY_NAME: config.get("intercom.company_name"),
     },
-};
+});

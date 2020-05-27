df479196c26136384b4bc399febc08f71d9753ec
Sarah Roberts <sarahr@cyverse.org>
2020-05-27 13:57:42 -0700

    CORE-994: added tests for tool listings

---
+++ src/__tests__/tools.js
@@ -0,0 +1,35 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import {
+    EmptyToolListingTest,
+    ErroredListingTest,
+    ToolListingTest,
+} from "../../stories/tools/Listing.stories";
+import { mockAxios } from "../../stories/axiosMock";
+
+beforeEach(() => {
+    mockAxios.reset();
+});
+
+afterEach(() => {
+    mockAxios.reset();
+});
+
+test("Tool Table View renders", () => {
+    const component = renderer.create(<ToolListingTest />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test("Tool Table View renders without tools", () => {
+    const component = renderer.create(<EmptyToolListingTest />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test("Errored Tool Listing renders", () => {
+    const component = renderer.create(<ErroredListingTest />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});

6486d48b2bd99ebc617960ae5667149ffb1c635a
John Wregglesworth <wregglej@cyverse.org>
2020-06-01 08:08:43 -0700

    Set the USER_SUFFIX so it's based on the IPLANT constant

--- src/components/dashboard/constants.js
+++ src/components/dashboard/constants.js
@@ -6,6 +6,8 @@
  * @module dashboard/constants
  */
 
+import meta from "../../constants";
+
 export const KIND_ANALYSES = "analyses";
 export const KIND_APPS = "apps";
 export const KIND_FEEDS = "feeds";
@@ -19,7 +21,7 @@ export const SECTION_NEWS = "news";
 export const SECTION_EVENTS = "events";
 export const SECTION_ITEM_LIMIT = 8;
 
-export const USER_SUFFIX = "@iplantcollaborative.org";
+export const USER_SUFFIX = `@${meta.IPLANT}.org`;
 export const CYVERSE = "CyVerse";
 export const DESC_MAX_LENGTH = 140;
 export const TITLE_MAX_LENGTH = 31;

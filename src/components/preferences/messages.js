d2e3ad2fbb29fa2b080f1df4d39fe6ce8a4f3cce
sriram srinivasan <sriram@cyverse.org>
2020-06-15 19:43:40 -0400

    Add i18n messages.

---
+++ src/components/preferences/messages.js
@@ -0,0 +1,20 @@
+export default {
+    locales: "en-us",
+    messages: {
+        generalHeaderLbl: "General",
+        rememberLastPathLbl: "Remember last file path for Apps",
+        hpcPrompt:
+            "Prompt for HPC apps authentication after log-on or when apps window is opened",
+        waitTimesWarning:
+            "Display Warning about wait times for submitting HPC apps",
+        defaultFolderHeaderLbl: "Default analysis output folder",
+        emailNotificationsHeaderLbl: "Email Notifications",
+        analysisStatusEmailLbl: "Email me when my analysis status changes",
+        urlImportEmailLbl: "Email me when my URL import status changes",
+        appsViewShortcutLbl: "Open apps view",
+        dataViewShortcutLbl: "Open data view",
+        analysesViewShortcutLbl: "Open analyses view",
+        notificationViewShortcutLbl: "Open notifications view",
+        keyboardShortcutHeaderLbl: "Keyboard Shortcuts",
+    },
+};

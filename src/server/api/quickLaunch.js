6acdb5507c4c88c977b8405a2c9c41af6a70762d
sriram srinivasan <sriram@cyverse.org>
2020-07-13 09:42:18 -0400

    Add QL handlers.

---
+++ src/server/api/quickLaunch.js
@@ -0,0 +1,34 @@
+/**
+ * @author sriram
+ *
+ * Add Quick Launch handler
+ *
+ */
+
+import express from "express";
+
+import * as auth from "../auth";
+import logger from "../logging";
+
+import { handler as terrainHandler } from "./terrain";
+
+export default function quickLaunchRouter() {
+    const api = express.Router();
+
+    logger.info("************ Adding Quick launch handlers **********");
+
+    logger.info("adding the POST /quicklaunches handler");
+    api.post(
+        "/quicklaunches",
+        auth.authnTokenMiddleware,
+        terrainHandler({
+            method: "POST",
+            pathname: "/quicklaunches",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+    );
+
+    return api;
+}

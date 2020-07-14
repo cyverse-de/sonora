7f09a7a16334432c252d12f75a28f4d7c8fe312a
Sriram Srinivasan <sriram@cyverse.org>
2020-07-14 12:31:20 -0400

    configure i18next for SB and JESt

--- src/__tests__/layout.js
+++ src/__tests__/layout.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import renderer from "react-test-renderer";
+import { I18nextProvider } from "react-i18next";
 import { AppBar } from "../../stories/CyVerseAppBar.stories";
+import i18n from "../test_i18n";
 
 test("App Bar renders", () => {
-    const component = renderer.create(<AppBar />);
+    const component = renderer.create(
+        <I18nextProvider i18n={i18n}>
+            <Suspense fallback="Loading...">
+                <AppBar />
+            </Suspense>
+        </I18nextProvider>
+    );
     component.unmount();
 });

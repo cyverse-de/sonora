5ae1eed6056707d0431cc6eb4f316b17da1db6f2
John Wregglesworth <wregglej@cyverse.org>
2020-07-27 09:29:12 -0700

    Add getInitialProps that lists the vice-admin namespace as required for i18n

--- src/pages/admin/vice.js
+++ src/pages/admin/vice.js
@@ -2,6 +2,15 @@ import React from "react";
 
 import VICEAdmin from "../../components/vice/admin";
 
+VICEAdminPage.getInitialProps = async ({ Component, ctx }) => {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps, namespacesRequired: ["vice-admin"] };
+};
+
 export default function VICEAdminPage() {
     return <VICEAdmin />;
 }

fd15b817f70ecf0ea234cfbda8cf15331b799f84
John Wregglesworth <wregglej@cyverse.org>
2020-08-05 14:21:10 -0700

    Adjust the namespacesRequired values for the dashboard page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Dashboard from "../components/Dashboard";
 
-export default function DashboardPage() {
+function DashboardPage() {
     return <Dashboard />;
 }
 
 DashboardPage.getInitialProps = async () => ({
     namespacesRequired: ["dashboard"],
 });
+
+export default DashboardPage;

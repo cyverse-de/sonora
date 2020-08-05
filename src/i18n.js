78c58e9e11c3cfb8669d4835a11027d3ed9da8b3
Ashley Ramsey <aramsey@iplantcollaborative.org>
2020-08-05 14:28:52 -0700

    Add localePath and otherLanguages to NextI18Next instance
    Following the example from https://github.com/isaachinman/next-i18next and added what was mentioned as required

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,8 @@
 const NextI18Next = require("next-i18next").default;
+const path = require("path");
 
 module.exports = new NextI18Next({
     defaultLanguage: "en",
+    otherLanguages: [],
+    localePath: path.resolve("./public/static/locales"),
 });

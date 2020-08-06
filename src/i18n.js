6ed6a120f77f7b855a50b96f7f22b01388ec5d79
Ashley Ramsey <aramsey@iplantcollaborative.org>
2020-08-05 17:12:48 -0700

    Refactor next-i18next instance, add I18nProviderWrapper component
    The refactor uses the updated import syntax and exports the functions needed for pages and components.  This also adds a wrapper component that will fix i18n errors seen in storybook and jest

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,50 @@
-const NextI18Next = require("next-i18next").default;
-const path = require("path");
+import NextI18Next from "next-i18next";
+import path from "path";
+import React, { Suspense } from "react";
+import { I18nextProvider } from "react-i18next";
 
-module.exports = new NextI18Next({
+/**
+ * The singular instance of next-i18next to be used throughout the app.
+ * Every page and component that needs i18n should use appWithTranslation,
+ * useTranslation, and withTranslation exported from this instance
+ *
+ * See examples here from the next-i18next repo:
+ * https://github.com/isaachinman/next-i18next/tree/master/examples/simple
+ */
+const {
+    i18n,
+    useTranslation,
+    withTranslation,
+    appWithTranslation,
+} = new NextI18Next({
     defaultLanguage: "en",
     otherLanguages: [],
     localePath: path.resolve("./public/static/locales"),
 });
+
+/**
+ * A wrapper component to provide i18n capabilities to Storybook as well as
+ * jest tests
+ *
+ * Copied suggestion from https://github.com/isaachinman/next-i18next/issues/324
+ *
+ * @param children
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function I18nProviderWrapper({ children }) {
+    return (
+        <Suspense fallback={"Loading..."}>
+            <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+        </Suspense>
+    );
+}
+
+// reexport everything
+export {
+    i18n,
+    useTranslation,
+    withTranslation,
+    appWithTranslation,
+    I18nProviderWrapper,
+};

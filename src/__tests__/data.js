89934b5140c50d28c06783016546aaf98ab5cbf4
Ashley Ramsey <aramsey@iplantcollaborative.org>
2020-08-10 10:21:46 -0700

    Add i18n wrapper around data tests

--- src/__tests__/data.js
+++ src/__tests__/data.js
@@ -1,8 +1,13 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { DataTableViewTest } from "../../stories/data/TableView.stories";
+import { I18nProviderWrapper } from "../i18n";
 
 test("Data Table View renders", () => {
-    const component = renderer.create(<DataTableViewTest />);
+    const component = renderer.create(
+        <I18nProviderWrapper>
+            <DataTableViewTest />
+        </I18nProviderWrapper>
+    );
     component.unmount();
 });

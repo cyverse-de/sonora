b0e1294e906d8da54f28aad5400ea83d0b62f2c5
Sriram Sirinvasan <sriram@on-campus-113-248.vpn.arizona.edu>
2020-08-13 14:54:46 -0400

    Remove file to change case.

--- src/components/apps/AppFields.js
+++
@@ -1,7 +0,0 @@
-export default {
-    NAME: { fieldName: "Name", key: "name" },
-    INTEGRATOR: { fieldName: "Integrated By", key: "integrator_name" },
-    RATING: { fieldName: "Rating", key: "average_rating" },
-    SYSTEM: { fieldName: "System Id", key: "system_id" },
-    STATUS: { fieldName: "", key: "status" },
-};

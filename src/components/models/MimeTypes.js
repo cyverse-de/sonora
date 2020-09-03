4f2e397f1b66dee699021acbde6167e89ec47080
Sriram Sirinvasan <sriram@on-campus-113-242.vpn.arizona.edu>
2020-09-03 10:06:09 -0400

    add logic to determine code-mirror mode.

--- src/components/models/MimeTypes.js
+++ src/components/models/MimeTypes.js
@@ -1,4 +1,4 @@
-export default {
+const mimeTypes = {
     PNG: "png",
     GIF: "gif",
     JPEG: "jpeg",
@@ -17,3 +17,28 @@ export default {
     X_PYTHON: "x-python",
     X_WEB_MARKDOWN: "x-web-markdown",
 };
+const getViewerMode = (mimeType) => {
+    let mode = null;
+    switch (mimeType) {
+        case mimeTypes.X_SH:
+            mode = "shell";
+            break;
+        case mimeTypes.X_PYTHON:
+            mode = "python";
+            break;
+        case mimeTypes.X_PERL:
+            mode = "perl";
+            break;
+        case mimeTypes.X_RSRC:
+            mode = "r";
+            break;
+        case mimeTypes.X_WEB_MARKDOWN:
+            mode = "markdown";
+            break;
+        default:
+            break  
+    }
+    return mode;
+};
+
+export { mimeTypes, getViewerMode };

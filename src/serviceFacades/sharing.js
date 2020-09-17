4be82e37d90a6c57a3389c1ed1d7aeb7e2cc08a6
Ashley Ramsey <aramsey@iplantcollaborative.org>
2020-09-16 18:08:27 -0700

    CORE-733 Add permission-lister calls and sharing req promises

--- src/serviceFacades/sharing.js
+++ src/serviceFacades/sharing.js
@@ -1,4 +1,9 @@
 import callApi from "../common/callApi";
+import { getResourcePermissions, RESOURCE_PERMISSIONS_KEY } from "./filesystem";
+import { getAppPermissions } from "./apps";
+import { getAnalysisPermissions } from "./analyses";
+import { getToolPermissions } from "./tools";
+export const GET_PERMISSIONS_QUERY_KEY = "fetchSharingPermissions";
 
 /**
  * @typedef DataSharingPath
@@ -90,5 +95,91 @@ export const searchSubjects = ({ searchTerm }) => {
     return callApi({
         endpoint: `/api/subjects`,
         params: { search: searchTerm.trim() },
+    });
+};
+
+export const doSharingUpdates = ({ sharing, unsharing }) => {
+    const { data, apps, analyses, tools } = sharing;
+    const {
+        data: dataUnshare,
+        apps: appsUnshare,
+        analyses: analysesUnshare,
+        tools: toolsUnshare,
+    } = unsharing;
+    let promises = [];
+    if (data && data.length > 0) {
+        promises.push(dataSharing({ sharingReq: { sharing: data } }));
+    }
+    if (dataUnshare && dataUnshare.length > 0) {
+        promises.push(
+            dataUnsharing({ unsharingReq: { unsharing: dataUnshare } })
+        );
+    }
+    if (apps && apps.length > 0) {
+        promises.push(shareApps({ appSharingRequest: { sharing: apps } }));
+    }
+    if (appsUnshare && appsUnshare.length > 0) {
+        promises.push(
+            unshareApps({ appUnsharingRequest: { unsharing: appsUnshare } })
+        );
+    }
+    if (analyses && analyses.length > 0) {
+        promises.push(
+            shareAnalyses({ analysisSharingRequest: { sharing: analyses } })
+        );
+    }
+    if (analysesUnshare && analysesUnshare.length > 0) {
+        promises.push(
+            unshareAnalyses({
+                analysisUnsharingRequest: { unsharing: analysesUnshare },
+            })
+        );
+    }
+    if (tools && tools.length > 0) {
+        promises.push(shareTools({ toolSharingRequest: { sharing: tools } }));
+    }
+    if (toolsUnshare && toolsUnshare.length > 0) {
+        promises.push(
+            unshareTools({ toolUnsharingRequest: { unsharing: toolsUnshare } })
+        );
+    }
+
+    return new Promise((resolve, reject) => {
+        Promise.all(promises)
+            .then((values) => {
+                resolve(values);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+export const getPermissions = (key, { resources }) => {
+    const { paths, apps, analyses, tools } = resources;
+    let permissionPromises = [];
+    if (paths && paths.length > 0) {
+        permissionPromises.push(
+            getResourcePermissions(RESOURCE_PERMISSIONS_KEY, { paths })
+        );
+    }
+    if (apps && apps.length > 0) {
+        permissionPromises.push(getAppPermissions({ apps }));
+    }
+    if (analyses && analyses.length > 0) {
+        permissionPromises.push(getAnalysisPermissions({ analyses }));
+    }
+    if (tools && tools.length > 0) {
+        permissionPromises.push(getToolPermissions({ tools }));
+    }
+
+    return new Promise((resolve, reject) => {
+        Promise.all(permissionPromises)
+            .then((values) => {
+                resolve(values);
+            })
+            .catch((error) => {
+                reject(error);
+            });
     });
 };

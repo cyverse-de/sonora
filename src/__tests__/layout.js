ee6a61908d1da20aab6e618b57b97120d4aab696
Sriram Sirinvasan <sriram@on-campus-113-232.vpn.arizona.edu>
2020-09-24 16:26:14 -0400

    Fix tests.

--- src/__tests__/layout.js
+++ src/__tests__/layout.js
@@ -2,11 +2,14 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { AppBar } from "../../stories/CyVerseAppBar.stories";
 import { I18nProviderWrapper } from "../i18n";
+import { PreferencesProvider } from "contexts/userPreferences";
 
 test("App Bar renders", () => {
     const component = renderer.create(
         <I18nProviderWrapper>
-            <AppBar />
+            <PreferencesProvider>
+                <AppBar />
+            </PreferencesProvider>
         </I18nProviderWrapper>
     );
     component.unmount();

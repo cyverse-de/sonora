c090ad233c36162685b84475bb7c519d33fa5930
Sriram Sirinvasan <sriram@on-campus-113-238.vpn.arizona.edu>
2020-09-25 14:53:10 -0400

    Add back and details button.

--- src/components/apps/launch/AppInfo.js
+++ src/components/apps/launch/AppInfo.js
@@ -6,11 +6,13 @@
 import React from "react";
 import { useTranslation } from "i18n";
 import { Trans } from "react-i18next";
+import { useRouter } from "next/router";
 
 import styles from "./styles";
 
 import { intercomShow } from "common/intercom";
 
+import DetailsDrawer from "components/apps/details/Drawer";
 import DEErrorDialog from "components/utils/error/DEErrorDialog";
 import ErrorTypography from "components/utils/error/ErrorTypography";
 
@@ -20,13 +22,17 @@ import { build as buildDebugId } from "@cyverse-de/ui-lib";
 
 import {
     Box,
+    Button,
     Hidden,
+    IconButton,
     Link,
     makeStyles,
     Typography,
     useMediaQuery,
     useTheme,
 } from "@material-ui/core";
+
+import { ArrowBack, Info } from "@material-ui/icons";
 
 import { Skeleton } from "@material-ui/lab";
 
@@ -105,25 +111,52 @@ const UnavailableMsg = ({ app, hasDeprecatedParams, baseId }) => {
 };
 const AppInfo = React.forwardRef((props, ref) => {
     const { app, baseId, hasDeprecatedParams, loading, loadingError } = props;
+    const { t } = useTranslation("common");
+    const router = useRouter();
     const classes = useStyles();
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+    const [open, setOpen] = React.useState(false);
     return (
         <div ref={ref}>
             <Typography
                 variant={isMobile ? "subtitle2" : "h6"}
                 className={classes.appInfoTypography}
             >
-                {loadingError ? (
-                    <LoadingErrorDisplay
-                        baseId={baseId}
-                        loadingError={loadingError}
-                    />
-                ) : loading ? (
-                    <Skeleton width={250} />
-                ) : (
-                    app?.name
-                )}
+                <>
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        size="small"
+                        style={{ marginRight: 8 }}
+                        startIcon={<ArrowBack fontSize="small" />}
+                        onClick={() => router.back()}
+                    >
+                        {t("back")}
+                    </Button>
+                    {loadingError ? (
+                        <LoadingErrorDisplay
+                            baseId={baseId}
+                            loadingError={loadingError}
+                        />
+                    ) : loading ? (
+                        <Skeleton width={250} />
+                    ) : (
+                        <>
+                            {app?.name}
+                            <IconButton onClick={() => setOpen(true)}>
+                                <Info color="primary" fontSize="small" />
+                            </IconButton>
+                            <DetailsDrawer
+                                appId={app?.id}
+                                systemId={app?.system_id}
+                                open={open}
+                                onClose={() => setOpen(false)}
+                                baseId="launch"
+                            />
+                        </>
+                    )}
+                </>
             </Typography>
             <Hidden xsDown>
                 <Typography

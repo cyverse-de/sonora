76f217cde1b5ed9ee1bf8ba6347897c13de0655b
flynn <abhyudaygaur@email.arizona.edu>
2020-10-08 14:06:17 -0700

    Add notification Icon in story file

--- stories/notifications/Notifications.stories.js
+++ stories/notifications/Notifications.stories.js
@@ -5,8 +5,11 @@
 
 import React from "react";
 import NotificationsMenu from "../../src/components/notifications/NotificationsMenu";
+import NotificationsIcon from '@material-ui/icons/Notifications';
 import notificationsData from "./notificationsData";
-import { mockAxios} from "../axiosMock";
+import {mockAxios} from "../axiosMock";
+import {Menu} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 export default {
     title: "Notifications",
@@ -15,9 +18,29 @@ export default {
 export const NotificationsPreviewTest = () => {
     mockAxios.onGet("/api/notifications/last-ten-messages").reply(200, notificationsData);
 
-     return (
-        <NotificationsMenu
-            baseDebugId="notificationWindow"
-        />
-     );
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <>
+            <NotificationsIcon aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}/>
+
+            <NotificationsMenu
+                baseDebugId="notificationWindow"
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                onClick={handleClick}
+                setAnchorEl={setAnchorEl}
+            />
+
+        </>
+
+    );
 };

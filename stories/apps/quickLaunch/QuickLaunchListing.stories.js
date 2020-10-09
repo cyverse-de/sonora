3352696df5c24bf0911b1398811a44b10ae2fad6
Sriram Sirinvasan <sriram@on-campus-113-254.vpn.arizona.edu>
2020-10-09 12:52:14 -0400

    Fix stories.

--- stories/apps/quickLaunch/QuickLaunchListing.stories.js
+++ stories/apps/quickLaunch/QuickLaunchListing.stories.js
@@ -1,136 +1,7 @@
 import React from "react";
 import ListQuickLaunches from "../../../src/components/apps/quickLaunch/QuickLaunchListing";
+import { quickLaunches } from "../AppMocks";
 import { mockAxios } from "../../axiosMock";
-const quickLaunches = [
-    {
-        id: "1",
-        name: "Qlaunch1",
-        description: "This is my first quick launch",
-        creator: "sriram@iplantcollaborative.org",
-        app_id: "456",
-        is_public: false,
-        submission: {
-            description: "string",
-            config: {},
-            "file-metadata": [
-                {
-                    attr: "string",
-                    value: "string",
-                    unit: "string",
-                },
-            ],
-            starting_step: 0,
-            name: "string",
-            app_id: "string",
-            system_id: "string",
-            debug: true,
-            create_output_subdir: true,
-            archive_logs: true,
-            output_dir: "string",
-            uuid: "string",
-            notify: true,
-            "skip-parent-meta": true,
-            callback: "string",
-            job_id: "string",
-        },
-    },
-    {
-        id: "2",
-        name: "Qlaunch2",
-        description: "This is my second quick launch",
-        creator: "ipctest@iplantcollaborative.org",
-        app_id: "456",
-        is_public: true,
-        submission: {
-            description: "string",
-            config: {},
-            "file-metadata": [
-                {
-                    attr: "string",
-                    value: "string",
-                    unit: "string",
-                },
-            ],
-            starting_step: 0,
-            name: "string",
-            app_id: "string",
-            system_id: "string",
-            debug: true,
-            create_output_subdir: true,
-            archive_logs: true,
-            output_dir: "string",
-            uuid: "string",
-            notify: true,
-            "skip-parent-meta": true,
-            callback: "string",
-            job_id: "string",
-        },
-    },
-    {
-        id: "3",
-        name: "Qlaunch3",
-        description: "This is my third quick launch",
-        creator: "sriram@iplantcollaborative.org",
-        app_id: "456",
-        is_public: true,
-        submission: {
-            description: "string",
-            config: {},
-            "file-metadata": [
-                {
-                    attr: "string",
-                    value: "string",
-                    unit: "string",
-                },
-            ],
-            starting_step: 0,
-            name: "string",
-            app_id: "string",
-            system_id: "string",
-            debug: true,
-            create_output_subdir: true,
-            archive_logs: true,
-            output_dir: "string",
-            uuid: "string",
-            notify: true,
-            "skip-parent-meta": true,
-            callback: "string",
-            job_id: "string",
-        },
-    },
-    {
-        id: "4",
-        name: "Qlaunch4",
-        description: "This is my fourth quick launch",
-        creator: "ipctest@iplantcollaborative.org",
-        app_id: "456",
-        is_public: false,
-        submission: {
-            description: "string",
-            config: {},
-            "file-metadata": [
-                {
-                    attr: "string",
-                    value: "string",
-                    unit: "string",
-                },
-            ],
-            starting_step: 0,
-            name: "string",
-            app_id: "string",
-            system_id: "string",
-            debug: true,
-            create_output_subdir: true,
-            archive_logs: true,
-            output_dir: "string",
-            uuid: "string",
-            notify: true,
-            "skip-parent-meta": true,
-            callback: "string",
-            job_id: "string",
-        },
-    },
-];
 
 export const QuickLaunchListingTest = () => {
     const [selected, setSelected] = React.useState(null);
@@ -162,7 +33,7 @@ export const QuickLaunchListingTest = () => {
             onDelete={onDelete}
             useQuickLaunch={useQuickLaunch}
             onCreate={onCreate}
-            baseDebugId="quicklaunchListing"
+            baseDebugId="quickLaunchListing"
             loading={false}
             selected={selected}
             onSelection={onSelection}

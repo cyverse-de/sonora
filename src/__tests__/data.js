66544db09b18ef6c0d0e3b62cbfe3d96d2ac7e75
Ian McEwen <mian@cyverse.org>
2020-11-12 14:43:05 -0700

    Make next dynamic imports work with jest, and dynamically load VideoViewer and TextViewer

--- src/__tests__/data.js
+++ src/__tests__/data.js
@@ -1,9 +1,14 @@
 import React from "react";
+import preloadAll from "jest-next-dynamic";
 import renderer from "react-test-renderer";
 import { DataTableViewTest } from "../../stories/data/TableView.stories";
 import { PathListFileViewerTest } from "../../stories/data/viewers/PathListViewer.stories";
 import { PlainTextFileViewerTest } from "../../stories/data/viewers/TextViewer.stories";
 import { I18nProviderWrapper } from "../i18n";
+
+beforeAll(async () => {
+    await preloadAll();
+});
 
 test("Data Table View renders", () => {
     const component = renderer.create(

7619df13c4c0d0ac1ed714b2c44d74abb84c3b77
Ashley Ramsey <aramsey@iplantcollaborative.org>
2020-11-23 16:13:00 -0700

    CORE-1009 Disable tag search for logged out users

--- src/components/data/TagSearch.js
+++ src/components/data/TagSearch.js
@@ -33,6 +33,7 @@ import styles from "./styles";
 import isQueryLoading from "../utils/isQueryLoading";
 import DEErrorDialog from "../utils/error/DEErrorDialog";
 import ErrorTypography from "../utils/error/ErrorTypography";
+import { useUserProfile } from "contexts/userProfile";
 
 const useStyles = makeStyles(styles);
 
@@ -50,6 +51,7 @@ function TagSearch(props) {
     const [errorMessage, setErrorMessage] = useState(null);
     const [errorDialogOpen, setErrorDialogOpen] = useState(false);
     const [errorObject, setErrorObject] = useState(null);
+    const [userProfile] = useUserProfile();
 
     const { isFetching: isFetchingTags } = useQuery({
         queryKey: fetchTagsQueryKey,
@@ -67,6 +69,7 @@ function TagSearch(props) {
         queryKey: { searchTerm },
         queryFn: getTagSuggestions,
         config: {
+            enabled: userProfile?.id,
             onSuccess: (resp) => {
                 setOptions(resp.tags);
             },

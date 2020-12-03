92f67dbbc51aa145bc7ffcf646af345bcd453910
Ashley Ramsey <aramsey@iplantcollaborative.org>
2020-12-03 14:00:31 -0700

    Disable analysis search for logged out users

--- src/__tests__/globalSearch.js
+++ src/__tests__/globalSearch.js
@@ -2,11 +2,14 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { SearchField } from "../../stories/search/GlobalSearchField.stories";
 import { I18nProviderWrapper } from "../i18n";
+import { UserProfileProvider } from "../contexts/userProfile";
 
 test("Search field renders", () => {
     const component = renderer.create(
         <I18nProviderWrapper>
-            <SearchField />
+            <UserProfileProvider>
+                <SearchField />
+            </UserProfileProvider>
         </I18nProviderWrapper>
     );
     component.unmount();

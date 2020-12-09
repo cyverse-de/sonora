8c5708ecf6f778d42dcc0925f687dba3e3043756
Sriram Sirinvasan <sriram@on-campus-113-238.vpn.arizona.edu>
2020-12-09 09:53:02 -0500

    Use DEDialog for SaveAsDialog.

--- src/components/data/SaveAsDialog.js
+++ src/components/data/SaveAsDialog.js
@@ -9,34 +9,21 @@ import React from "react";
 import { Field, Form, Formik } from "formik";
 
 import { useTranslation } from "react-i18next";
-import ids from "./ids";
-import SaveAsField from "./SaveAsField";
-import styles from "./styles";
-import ResourceTypes from "components/models/ResourceTypes";
-import InputSelector from "components/apps/launch/InputSelector";
 
 import { build } from "@cyverse-de/ui-lib";
 
-import { makeStyles } from "@material-ui/core/styles";
-import { Close } from "@material-ui/icons";
-
+import ids from "./ids";
+import SaveAsField from "./SaveAsField";
+import ResourceTypes from "components/models/ResourceTypes";
+import InputSelector from "components/apps/launch/InputSelector";
+import DEDialog from "components/utils/DEDialog";
 import { validateDiskResourceName } from "./utils";
 
-import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    IconButton,
-} from "@material-ui/core";
-
-const useStyles = makeStyles(styles);
+import { Button } from "@material-ui/core";
 
 function SaveAsDialog(props) {
     const { path, open, onClose, onSaveAs, saveFileError, loading } = props;
 
-    const classes = useStyles();
     const baseId = ids.CREATE_DLG;
 
     const { t } = useTranslation("data");
@@ -68,63 +55,50 @@ function SaveAsDialog(props) {
             {({ handleSubmit, validateForm }) => {
                 return (
                     <Form>
-                        <Dialog
+                        <DEDialog
                             open={open}
                             onClose={onClose}
-                            maxWidth="sm"
-                            fullWidth
+                            baseId={baseId}
+                            title={i18nCommon("saveAs")}
+                            actions={
+                                <>
+                                    <Button
+                                        id={build(baseId, ids.CANCEL_BTN)}
+                                        onClick={onClose}
+                                    >
+                                        {i18nCommon("cancel")}
+                                    </Button>
+                                    <Button
+                                        id={build(baseId, ids.CREATE_BTN)}
+                                        color="primary"
+                                        type="submit"
+                                        onClick={handleSubmit}
+                                    >
+                                        {i18nCommon("save")}
+                                    </Button>
+                                </>
+                            }
                         >
-                            <DialogTitle>
-                                {i18nCommon("saveAs")}
-                                <IconButton
-                                    aria-label={i18nCommon("cancel")}
-                                    onClick={onClose}
-                                    size="small"
-                                    edge="end"
-                                    classes={{ root: classes.closeButton }}
-                                >
-                                    <Close />
-                                </IconButton>
-                            </DialogTitle>
-                            <DialogContent>
-                                <Field
-                                    startingPath={path}
-                                    name="dest"
-                                    id={build(baseId, ids.PATH)}
-                                    acceptedType={ResourceTypes.FOLDER}
-                                    label={t("newFileLocation")}
-                                    component={InputSelector}
-                                    required={true}
-                                />
-                                <Field
-                                    id={build(baseId, ids.FILE_NAME)}
-                                    name="name"
-                                    required={true}
-                                    label={t("fileName")}
-                                    loading={loading}
-                                    path={path}
-                                    component={SaveAsField}
-                                    {...textFieldProps}
-                                />
-                            </DialogContent>
-
-                            <DialogActions>
-                                <Button
-                                    id={build(baseId, ids.CANCEL_BTN)}
-                                    onClick={onClose}
-                                >
-                                    {i18nCommon("cancel")}
-                                </Button>
-                                <Button
-                                    id={build(baseId, ids.CREATE_BTN)}
-                                    color="primary"
-                                    type="submit"
-                                    onClick={handleSubmit}
-                                >
-                                    {i18nCommon("save")}
-                                </Button>
-                            </DialogActions>
-                        </Dialog>
+                            <Field
+                                startingPath={path}
+                                name="dest"
+                                id={build(baseId, ids.PATH)}
+                                acceptedType={ResourceTypes.FOLDER}
+                                label={t("newFileLocation")}
+                                component={InputSelector}
+                                required={true}
+                            />
+                            <Field
+                                id={build(baseId, ids.FILE_NAME)}
+                                name="name"
+                                required={true}
+                                label={t("fileName")}
+                                loading={loading}
+                                path={path}
+                                component={SaveAsField}
+                                {...textFieldProps}
+                            />
+                        </DEDialog>
                     </Form>
                 );
             }}

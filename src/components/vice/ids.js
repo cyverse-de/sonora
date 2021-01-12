3f27145285520381d007e38def19051d8ceb2e88
Sriram Sirinvasan <sriram@cyverse.org>
2021-01-12 14:07:36 -0500

    Add VICE  request dialog

---
+++ src/components/vice/ids.js
@@ -0,0 +1,15 @@
+export default {
+    ACCESS_REQUEST_DLG: {
+        NAME: "name",
+        EMAIL: "email",
+        ORCID: "orcid",
+        AFFILIATION: "affiliation",
+        AFF_DETAILS: "affDetails",
+        USE_CASE: "useCase",
+        DETAILS: "details",
+        SUBMIT: "submit",
+        CANCEL: "cancel",
+        UNIVERSITY: "university",
+        OTHER: "other",
+    },
+};

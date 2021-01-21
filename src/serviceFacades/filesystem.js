ea5d6e4c6452f1ba89587df2428281c842da5d8d
Sriram Sirinvasan <sriram@cyverse.org>
2021-01-21 16:56:47 -0500

    Add restore and empty trash end-points and handlers.

--- src/serviceFacades/filesystem.js
+++ src/serviceFacades/filesystem.js
@@ -286,3 +286,20 @@ export const pathListCreator = ({
         params,
     });
 };
+
+export const restore = (paths) => {
+    return callApi({
+        endpoint: "/api/filesystem/restore",
+        method: "POST",
+        body: {
+            paths,
+        },
+    });
+};
+
+export const emptyTrash = () => {
+    return callApi({
+        endpoint: "/api/filesystem/trash",
+        method: "DELETE",
+    });
+};

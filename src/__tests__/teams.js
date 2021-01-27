d57687823a9fbdbbfee0183b35cd3ac50ac6c9c9
Ashley Ramsey <aramsey@iplantcollaborative.org>
2021-01-27 09:25:14 -0700

    CORE-827 Add logic and tests for combining team privileges and members

--- src/__tests__/teams.js
+++ src/__tests__/teams.js
@@ -5,6 +5,8 @@ import { View, SearchResults } from "../../stories/teams/Teams.stories";
 import { mockAxios } from "../../stories/axiosMock";
 import { UserProfileProvider } from "contexts/userProfile";
 import { I18nProviderWrapper } from "../i18n";
+import { getAllPrivileges } from "../components/teams/util";
+import Privilege from "../components/models/Privilege";
 
 beforeEach(() => {
     mockAxios.reset();
@@ -34,4 +36,82 @@ test("Team search results renders", () => {
         </UserProfileProvider>
     );
     component.unmount();
+});
+
+function createPrivilegeList(privileges, userId) {
+    const a = [];
+    const b = [];
+    privileges.forEach(([before, after], index) => {
+        a.push({
+            name: before.value,
+            subject: {
+                id: userId || `${index}${before.value}`,
+            },
+        });
+        after &&
+            b.push({
+                name: after.value,
+                subject: {
+                    id: userId || `${index}${before.value}`,
+                },
+            });
+    });
+    return [a, b];
+}
+
+it("tests that the grouper admin account is removed", () => {
+    const [before, after] = createPrivilegeList(
+        [[Privilege.ADMIN, null]],
+        "de_grouper"
+    );
+    const result = Object.values(getAllPrivileges(before));
+
+    expect(result).toStrictEqual(after);
+});
+
+it("tests that members only have `read` or `admin` privileges", () => {
+    const [before, after] = createPrivilegeList([
+        [Privilege.OPTOUT, Privilege.READ],
+        [Privilege.OPTIN, Privilege.READ],
+        [Privilege.VIEW, Privilege.READ],
+        [Privilege.READ, Privilege.READ],
+        [Privilege.ADMIN, Privilege.ADMIN],
+    ]);
+    const result = Object.values(getAllPrivileges(before));
+
+    expect(result).toStrictEqual(after);
+});
+
+it("tests that public privileges don't go above `view`", () => {
+    const [before, after] = createPrivilegeList(
+        [[Privilege.READ, Privilege.VIEW]],
+        "GrouperAll"
+    );
+
+    const result = Object.values(getAllPrivileges(before));
+    expect(result).toStrictEqual(after);
+});
+
+it("tests that highest privilege wins", () => {
+    const [before, after] = createPrivilegeList(
+        [
+            [Privilege.OPTOUT, Privilege.ADMIN],
+            [Privilege.ADMIN, null],
+        ],
+        "batman"
+    );
+
+    const result = Object.values(getAllPrivileges(before));
+    expect(result).toStrictEqual(after);
+});
+
+it("tests that missing member gets default member privilege", () => {
+    const member = {
+        id: "batman",
+    };
+
+    const after = [{ name: "read", subject: { ...member } }];
+
+    const result = Object.values(getAllPrivileges([], [member]));
+    expect(result).toStrictEqual(after);
 });
---
+++ src/components/teams/util.js
@@ -0,0 +1,107 @@
+import Privilege from "../models/Privilege";
+
+export const DEFAULT_MEMBER_PRIVILEGE = Privilege.READ.value;
+
+export const privilegeHasRead = (privilege) => {
+    return privilegeNum(privilege) >= Privilege.READ.num;
+};
+
+export const privilegeIsAdmin = (privilege) => {
+    return privilegeNum(privilege) === Privilege.ADMIN.num;
+};
+
+const restrictedGroupNameChars = ":_";
+
+export function validateGroupName(value, t) {
+    if (!value || value.length < 1) {
+        return "Empty value";
+    }
+
+    const restrictedRegex = new RegExp(
+        "[" + restrictedGroupNameChars + "]",
+        "g"
+    );
+    const invalid = value.match(restrictedRegex);
+    if (invalid) {
+        return t("invalidTeamName", {
+            invalidCharList: restrictedGroupNameChars,
+            invalidChars: invalid.join(""),
+        });
+    }
+}
+
+function privilegeNum(privilege) {
+    return privilege ? Privilege[privilege.name.toUpperCase()].num : 0;
+}
+
+function getDefaultPrivilege(privilege) {
+    return { ...privilege, name: DEFAULT_MEMBER_PRIVILEGE };
+}
+
+/**
+ * It's possible via the API to assign multiple privileges to the same person, however,
+ * it's only logical to have one permission as they mostly build on top of each
+ * other.
+ *
+ * For Sonora, we have simplified privileges to only ADMIN or READ for members,
+ * VIEW for public privileges or nothing if the team is private.
+ *
+ * The grouper admin privilege is removed so users don't try to modify it.
+ *
+ * @param privileges - an array of privilege objects expected to have a name key with a
+ * value from {@link Privilege}, as well as a subject key with
+ * a value containing a subject object
+ */
+function simplifyPrivileges(privileges) {
+    return privileges.reduce((acc, privilege) => {
+        const subjectId = privilege.subject.id;
+        if (subjectId === "de_grouper") {
+            return acc;
+        }
+
+        const currentPriv = acc[subjectId] || getDefaultPrivilege(privilege);
+
+        return {
+            ...acc,
+            [subjectId]:
+                subjectId === "GrouperAll"
+                    ? { ...privilege, name: Privilege.VIEW.value }
+                    : privilegeNum(currentPriv) > privilegeNum(privilege)
+                    ? currentPriv
+                    : privilege,
+        };
+    }, {});
+}
+
+/**
+ * It's possible via the API to add a member who has no privileges.
+ * The API also returns the most succinct list of privileges - e.g. if public
+ * privileges are set to VIEW and a member also has VIEW, that member will
+ * not be returned from the GET /team/[teamName]/privileges endpoint because
+ * it's already implied by the public privilege.
+ *
+ * For sonora, members will by default have READ unless they have ADMIN
+ *
+ * @param privilegeMap
+ * @param members
+ * @return
+ */
+function addMembersToPrivileges(privilegeMap, members) {
+    members &&
+        members.forEach((member) => {
+            const subjectId = member.id;
+            if (!privilegeMap[subjectId]) {
+                privilegeMap[subjectId] = {
+                    name: DEFAULT_MEMBER_PRIVILEGE,
+                    subject: { ...member },
+                };
+            }
+        });
+
+    return privilegeMap;
+}
+
+export function getAllPrivileges(privileges, members) {
+    const privilegeMap = simplifyPrivileges(privileges);
+    return addMembersToPrivileges(privilegeMap, members);
+}

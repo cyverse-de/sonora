f5fc1f7a5194c3e700b18788a02e57ea3c6af6c5
Ian McEwen <mian@cyverse.org>
2021-01-28 09:20:30 -0700

    Amend AnalysesIcon to 20px circle in 24px canvas

--- src/components/icons/AnalysesIcon.js
+++ src/components/icons/AnalysesIcon.js
@@ -7,15 +7,21 @@ function SvgAnalyses(props) {
             xmlns="http://www.w3.org/2000/svg"
             width="1em"
             height="1em"
-            viewBox="0 0 28 28"
+            viewBox="0 0 24 24"
             {...props}
         >
             <g fill="currentColor">
-                <path d="M17.579 10.791h3.615v14.452h-3.615zM12.106 12.925h3.407v13.618h-3.407zM6.96 17.016h3.069v7.47H6.96z" />
+                <path d="M14.556 9.708h2.583V20.03h-2.583zM10.647 11.232h2.434v9.727h-2.434zM6.971 14.154h2.193v5.336H6.97z" />
             </g>
             <g fill="none" stroke="currentColor" strokeWidth={3}>
-                <circle cx={14} cy={14} r={14} stroke="none" />
-                <circle cx={14} cy={14} r={12.5} />
+                <path
+                    d="M22 12a10 10 0 01-10 10A10 10 0 012 12 10 10 0 0112 2a10 10 0 0110 10z"
+                    stroke="none"
+                />
+                <path
+                    d="M20.929 12A8.929 8.929 0 0112 20.929 8.929 8.929 0 013.071 12 8.929 8.929 0 0112 3.071 8.929 8.929 0 0120.929 12z"
+                    strokeWidth={2.14287}
+                />
             </g>
         </svg>
     );

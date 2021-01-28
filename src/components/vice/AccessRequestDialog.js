289f67c8ad10bacc920414b58ea5cd03127e2fd4
Sriram Sirinvasan <sriram@cyverse.org>
2021-01-28 13:33:48 -0500

    Remove unnecessary fields. Add validation.

--- src/components/vice/AccessRequestDialog.js
+++ src/components/vice/AccessRequestDialog.js
@@ -5,175 +5,64 @@
  */
 import React from "react";
 import { useTranslation } from "i18n";
-
+import { useMutation } from "react-query";
 import ids from "./ids";
-import {
-    build,
-    FormMultilineTextField,
-    FormSelectField,
-    FormTextField,
-} from "@cyverse-de/ui-lib";
+import { build, FormMultilineTextField } from "@cyverse-de/ui-lib";
 import { Field, Form, Formik } from "formik";
 
-import { useUserProfile } from "contexts/userProfile";
+import requestAccess from "serviceFacades/vice/accessRequest";
+import constants from "../../constants";
+
 import DEDialog from "components/utils/DEDialog";
-import { Button, Grid, MenuItem, Typography } from "@material-ui/core";
+import { Button, CircularProgress, Typography } from "@material-ui/core";
+
+const MIN_USE_CASE_LENGTH = 60;
 
 export default function AccessRequestDialog(props) {
     const { open, baseId, onClose } = props;
     const { t } = useTranslation("vice");
     const { t: i18nCommon } = useTranslation("common");
-    const [userProfile] = useUserProfile();
+
+    const [submitRequest, { status }] = useMutation(requestAccess, {
+        onSuccess: (data) => {
+            console.log("request submitted");
+            onClose();
+        },
+        onError: (err) => console.log("Unable to submit request" + err),
+    });
+
     const handleSubmit = (values) => {
         console.log("submitting values=>" + values);
+        submitRequest(values);
+    };
+    const validate = (value) => {
+        if (!value) {
+            return t("emptyValue");
+        }
+        if (value.length < MIN_USE_CASE_LENGTH) {
+            return t("minLengthUseCaseError", {
+                minLength: MIN_USE_CASE_LENGTH,
+            });
+        }
     };
     return (
-        <DEDialog
-            open={open}
-            onClose={onClose}
-            baseId={baseId}
-            title={t("accessRequestTitle")}
+        <Formik
+            onSubmit={handleSubmit}
+            enableReinitialize={true}
+            initialValues={{
+                intended_use: "",
+            }}
         >
-            <Typography style={{ margin: 8 }}>
-                {t("accessRequestPrompt")}
-            </Typography>
-            <Formik
-                onSubmit={handleSubmit}
-                enableReinitialize={true}
-                initialValues={{
-                    name: userProfile?.attributes.name,
-                    email: userProfile?.attributes.email,
-                }}
-            >
-                {({ values }) => {
-                    return (
-                        <Form>
-                            <Field
-                                name="name"
-                                label={t("name")}
-                                id={build(baseId, ids.ACCESS_REQUEST_DLG.NAME)}
-                                required
-                                component={FormTextField}
-                                InputProps={{
-                                    readOnly: true,
-                                }}
-                            />
-                            <Field
-                                name="email"
-                                label={t("email")}
-                                id={build(baseId, ids.ACCESS_REQUEST_DLG.EMAIL)}
-                                required
-                                component={FormTextField}
-                                InputProps={{
-                                    readOnly: true,
-                                }}
-                            />
-                            <Field
-                                name="orcid"
-                                label={t("orcid")}
-                                id={build(baseId, ids.ACCESS_REQUEST_DLG.ORCID)}
-                                required
-                                component={FormTextField}
-                                helperText={t("orcidHelp")}
-                            />
-                            <Field name="institution">
-                                {({
-                                    field: { onChange, ...field },
-                                    ...props
-                                }) => (
-                                    <FormSelectField
-                                        {...props}
-                                        label={t("Affiliation")}
-                                        required
-                                        field={field}
-                                        onChange={(event) => {
-                                            onChange(event);
-                                        }}
-                                        id={build(baseId, "aff")}
-                                    >
-                                        <MenuItem
-                                            key="univ"
-                                            value="University"
-                                            id={build(
-                                                baseId,
-                                                ids.ACCESS_REQUEST_DLG
-                                                    .UNIVERSITY
-                                            )}
-                                        >
-                                            {t("univ")}
-                                        </MenuItem>
-                                        <MenuItem
-                                            key="nih"
-                                            value="nih"
-                                            id={build(
-                                                baseId,
-                                                ids.ACCESS_REQUEST_DLG
-                                                    .UNIVERSITY
-                                            )}
-                                        >
-                                            {t("nih")}
-                                        </MenuItem>
-                                        <MenuItem
-                                            key="nsf"
-                                            value="nsf"
-                                            id={build(
-                                                baseId,
-                                                ids.ACCESS_REQUEST_DLG
-                                                    .UNIVERSITY
-                                            )}
-                                        >
-                                            {t("nsf")}
-                                        </MenuItem>
-                                        <MenuItem
-                                            key="other"
-                                            value="Other"
-                                            id={build(
-                                                baseId,
-                                                ids.ACCESS_REQUEST_DLG.OTHER
-                                            )}
-                                        >
-                                            {t("others")}
-                                        </MenuItem>
-                                    </FormSelectField>
-                                )}
-                            </Field>
-                            <Field
-                                name="affDetails"
-                                label={t("moreDetails")}
-                                id={build(
-                                    baseId,
-                                    ids.ACCESS_REQUEST_DLG.AFF_DETAILS
-                                )}
-                                required
-                                component={FormMultilineTextField}
-                            />
-                            <Field
-                                name="intended_use"
-                                label={t("useCase")}
-                                id={build(
-                                    baseId,
-                                    ids.ACCESS_REQUEST_DLG.USE_CASE
-                                )}
-                                required
-                                component={FormMultilineTextField}
-                            />
-                            <Field
-                                name="details"
-                                label={t("otherDetails")}
-                                id={build(
-                                    baseId,
-                                    ids.ACCESS_REQUEST_DLG.DETAILS
-                                )}
-                                component={FormMultilineTextField}
-                            />
-                            <Grid
-                                container
-                                direction="row"
-                                justify="flex-end"
-                                alignItems="flex-end"
-                                spacing={1}
-                            >
-                                <Grid item>
+            {({ handleSubmit, values }) => {
+                return (
+                    <Form>
+                        <DEDialog
+                            open={open}
+                            onClose={onClose}
+                            baseId={baseId}
+                            title={t("accessRequestTitle")}
+                            actions={
+                                <>
                                     <Button
                                         id={build(
                                             baseId,
@@ -183,8 +72,6 @@ export default function AccessRequestDialog(props) {
                                     >
                                         {i18nCommon("cancel")}
                                     </Button>
-                                </Grid>
-                                <Grid item>
                                     <Button
                                         id={build(
                                             baseId,
@@ -192,15 +79,42 @@ export default function AccessRequestDialog(props) {
                                         )}
                                         type="submit"
                                         color="primary"
+                                        onClick={handleSubmit}
                                     >
                                         {i18nCommon("submit")}
                                     </Button>
-                                </Grid>
-                            </Grid>
-                        </Form>
-                    );
-                }}
-            </Formik>
-        </DEDialog>
+                                </>
+                            }
+                        >
+                            <Typography style={{ margin: 8 }}>
+                                {t("accessRequestPrompt")}
+                            </Typography>
+                            {status === constants.LOADING && (
+                                <CircularProgress
+                                    size={30}
+                                    thickness={5}
+                                    style={{
+                                        position: "absolute",
+                                        top: "50%",
+                                        left: "50%",
+                                    }}
+                                />
+                            )}
+                            <Field
+                                name="intended_use"
+                                label={t("useCase")}
+                                id={build(
+                                    baseId,
+                                    ids.ACCESS_REQUEST_DLG.USE_CASE
+                                )}
+                                required
+                                component={FormMultilineTextField}
+                                validate={validate}
+                            />
+                        </DEDialog>
+                    </Form>
+                );
+            }}
+        </Formik>
     );
 }

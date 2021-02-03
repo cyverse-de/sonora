750084d5967c0980ea9499a0378c6625344d4706
Sriram Sirinvasan <sriram@cyverse.org>
2021-02-03 14:10:18 -0500

    Add toolbar.

--- src/components/doi/Toolbar.js
+++ src/components/doi/Toolbar.js
@@ -4,7 +4,7 @@
  * A toolbar with actions for admin DOI Request listing
  *
  */
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "i18n";
 
 import ids from "./ids";
@@ -20,6 +20,7 @@ import {
 import UpdateIcon from "@material-ui/icons/Update";
 import ViewListIcon from "@material-ui/icons/ViewList";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
+import UpdateRequestDialog from "./UpdateRequestDialog";
 
 const useStyles = makeStyles((theme) => ({
     divider: {
@@ -37,71 +38,80 @@ export default function DOIToolbar(props) {
     const toolbarId = build(baseId, ids.toolbarId);
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+    const [updateDialogOpen, setUpdateDialogOpen] = useState();
     return (
-        <Toolbar variant="dense" id={toolbarId}>
-            {!isMobile && selected && (
-                <>
-                    <div className={classes.divider} />
-                    <Button
-                        id={build(toolbarId, ids.UPDATE_REQUEST_BTN)}
-                        startIcon={<UpdateIcon />}
-                        color="primary"
-                        size="small"
-                        variant="outlined"
-                        className={classes.toolbarItems}
-                    >
-                        {t("updateRequest")}
-                    </Button>
-                    <Button
-                        id={build(toolbarId, ids.VIEW_METADATA_BTN)}
-                        startIcon={<ViewListIcon />}
-                        color="primary"
-                        size="small"
-                        variant="outlined"
-                        className={classes.toolbarItems}
-                    >
-                        {t("viewMetadata")}
-                    </Button>
-                    <Button
-                        id={build(toolbarId, ids.CREATE_DOI_BTN)}
-                        startIcon={<AddCircleIcon />}
-                        color="primary"
-                        size="small"
-                        variant="outlined"
-                        className={classes.toolbarItems}
-                    >
-                        {t("createDoi")}
-                    </Button>
-                </>
-            )}
-            {isMobile && selected && (
-                <>
-                    <IconButton
-                        id={build(toolbarId, ids.UPDATE_REQUEST_BTN)}
-                        color="primary"
-                        variant="outlined"
-                        className={classes.toolbarItems}
-                    >
-                        <UpdateIcon />
-                    </IconButton>
-                    <IconButton
-                        id={build(toolbarId, ids.VIEW_METADATA_BTN)}
-                        color="primary"
-                        variant="outlined"
-                        className={classes.toolbarItems}
-                    >
-                        <ViewListIcon />
-                    </IconButton>
-                    <IconButton
-                        id={build(toolbarId, ids.CREATE_DOI_BTN)}
-                        color="primary"
-                        variant="outlined"
-                        className={classes.toolbarItems}
-                    >
-                        <AddCircleIcon />
-                    </IconButton>
-                </>
-            )}
-        </Toolbar>
+        <>
+            <Toolbar variant="dense" id={toolbarId}>
+                {!isMobile && selected && (
+                    <>
+                        <div className={classes.divider} />
+                        <Button
+                            id={build(toolbarId, ids.UPDATE_REQUEST_BTN)}
+                            startIcon={<UpdateIcon />}
+                            color="primary"
+                            size="small"
+                            variant="outlined"
+                            className={classes.toolbarItems}
+                            onClick={() => setUpdateDialogOpen(true)}
+                        >
+                            {t("updateRequest")}
+                        </Button>
+                        <Button
+                            id={build(toolbarId, ids.VIEW_METADATA_BTN)}
+                            startIcon={<ViewListIcon />}
+                            color="primary"
+                            size="small"
+                            variant="outlined"
+                            className={classes.toolbarItems}
+                        >
+                            {t("viewMetadata")}
+                        </Button>
+                        <Button
+                            id={build(toolbarId, ids.CREATE_DOI_BTN)}
+                            startIcon={<AddCircleIcon />}
+                            color="primary"
+                            size="small"
+                            variant="outlined"
+                            className={classes.toolbarItems}
+                        >
+                            {t("createDoi")}
+                        </Button>
+                    </>
+                )}
+                {isMobile && selected && (
+                    <>
+                        <IconButton
+                            id={build(toolbarId, ids.UPDATE_REQUEST_BTN)}
+                            color="primary"
+                            variant="outlined"
+                            className={classes.toolbarItems}
+                            onClick={() => setUpdateDialogOpen(true)}
+                        >
+                            <UpdateIcon />
+                        </IconButton>
+                        <IconButton
+                            id={build(toolbarId, ids.VIEW_METADATA_BTN)}
+                            color="primary"
+                            variant="outlined"
+                            className={classes.toolbarItems}
+                        >
+                            <ViewListIcon />
+                        </IconButton>
+                        <IconButton
+                            id={build(toolbarId, ids.CREATE_DOI_BTN)}
+                            color="primary"
+                            variant="outlined"
+                            className={classes.toolbarItems}
+                        >
+                            <AddCircleIcon />
+                        </IconButton>
+                    </>
+                )}
+            </Toolbar>
+            <UpdateRequestDialog
+                open={updateDialogOpen}
+                onClose={() => setUpdateDialogOpen(false)}
+            />
+        </>
     );
 }

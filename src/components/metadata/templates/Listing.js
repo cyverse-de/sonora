bc5c10bd99cf2ea9c95131c9ec6b9bd851f92e4a
Paul Sarando <psarando@iplantcollaborative.org>
2021-02-05 17:28:09 -0700

    CORE-1212 Refactor Select Template dialog from Table to List

--- src/components/metadata/templates/Listing.js
+++ src/components/metadata/templates/Listing.js
@@ -5,7 +5,6 @@ import React from "react";
 import { useQuery } from "react-query";
 
 import { useTranslation } from "i18n";
-import constants from "../../../constants";
 
 import ids from "../ids";
 
@@ -13,7 +12,6 @@ import SlideUpTransition from "../SlideUpTransition";
 
 import DEDialog from "components/utils/DEDialog";
 import { getHost } from "components/utils/getHost";
-import TableLoading from "components/utils/TableLoading";
 import WrappedErrorHandler from "components/utils/error/WrappedErrorHandler";
 
 import {
@@ -21,107 +19,49 @@ import {
     getFilesystemMetadataTemplateListing,
 } from "serviceFacades/metadata";
 
-import {
-    build,
-    EnhancedTableHead,
-    getSorting,
-    stableSort,
-} from "@cyverse-de/ui-lib";
+import { build } from "@cyverse-de/ui-lib";
 
 import {
-    ButtonGroup,
     Button,
-    Paper,
-    Popover,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableRow,
-    Typography,
-    makeStyles,
+    IconButton,
+    List,
+    ListItem,
+    ListItemSecondaryAction,
+    ListItemText,
 } from "@material-ui/core";
 
-import { CloudDownload, Info } from "@material-ui/icons";
+import { CloudDownload } from "@material-ui/icons";
 
-const getColumnData = (t) => [
-    {
-        id: build(ids.COL_HEADER, ids.METADATA_TEMPLATE_VIEW),
-        key: "name",
-        name: t("templateNameLabel"),
-        enableSorting: true,
-    },
-    {
-        id: build(ids.COL_HEADER, ids.COL_ACTIONS),
-        key: "actions",
-        enableSorting: false,
-        padding: "none",
-    },
-];
+import Skeleton from "@material-ui/lab/Skeleton";
 
-const useStyles = makeStyles((theme) => ({
-    templateInfo: {
-        padding: theme.spacing(2),
-    },
-}));
-
-const TemplateInfoDisplay = (props) => {
-    const { templateInfo, anchorEl, onClose } = props;
-
-    const classes = useStyles();
-
-    return (
-        <Popover
-            open={!!anchorEl}
-            anchorEl={anchorEl}
-            onClose={onClose}
-            anchorOrigin={{
-                vertical: "center",
-                horizontal: "left",
-            }}
-            transformOrigin={{
-                vertical: "center",
-                horizontal: "right",
-            }}
-        >
-            <Typography className={classes.templateInfo}>
-                {templateInfo}
-            </Typography>
-        </Popover>
-    );
-};
-
-const TemplateTableRow = (props) => {
-    const { baseId, template, selected, onClick, onInfo, onDownload } = props;
+const TemplateListItem = (props) => {
+    const { baseId, template, selected, onClick, onDownload } = props;
 
     const { t } = useTranslation("metadata");
 
     return (
-        <TableRow hover selected={selected} onClick={onClick}>
-            <TableCell component="th" scope="row">
-                {template.name}
-            </TableCell>
-            <TableCell align="right">
-                <ButtonGroup variant="text">
-                    {template.description && (
-                        <Button
-                            id={build(baseId, ids.BUTTONS.INFO)}
-                            aria-label={t("templateDescription")}
-                            onClick={onInfo}
-                        >
-                            <Info />
-                        </Button>
-                    )}
-                    <Button
-                        id={build(baseId, ids.BUTTONS.DOWNLOAD)}
-                        aria-label={t("downloadTemplate")}
-                        onClick={onDownload}
-                    >
-                        <CloudDownload />
-                    </Button>
-                </ButtonGroup>
-            </TableCell>
-        </TableRow>
+        <ListItem
+            id={baseId}
+            divider
+            button
+            selected={selected}
+            onClick={onClick}
+        >
+            <ListItemText
+                primary={template.name}
+                secondary={template.description}
+            />
+            <ListItemSecondaryAction>
+                <IconButton
+                    id={build(baseId, ids.BUTTONS.DOWNLOAD)}
+                    aria-label={t("downloadTemplate")}
+                    color="primary"
+                    onClick={onDownload}
+                >
+                    <CloudDownload />
+                </IconButton>
+            </ListItemSecondaryAction>
+        </ListItem>
     );
 };
 
@@ -132,10 +72,6 @@ const MetadataTemplateListing = (props) => {
 
     const [templates, setTemplates] = React.useState(null);
     const [selectedTemplateId, setSelectedTemplateId] = React.useState(null);
-    const [orderBy, setOrderBy] = React.useState(null);
-    const [order, setOrder] = React.useState(constants.SORT_ASCENDING);
-    const [templateInfoAnchor, setTemplateInfoAnchor] = React.useState(null);
-    const [templateInfo, setTemplateInfo] = React.useState(null);
     const [downloadTemplateId, setDownloadTemplateId] = React.useState(null);
 
     React.useEffect(() => {
@@ -159,32 +95,14 @@ const MetadataTemplateListing = (props) => {
         },
     });
 
-    const handleRequestSort = (event, property) => {
-        let newOrder = constants.SORT_ASCENDING;
-
-        if (orderBy === property && order === newOrder) {
-            newOrder = constants.SORT_DESCENDING;
-        }
-
-        setOrder(newOrder);
-        setOrderBy(property);
-    };
-
-    const dialogID = build(baseId, ids.DIALOG, ids.METADATA_TEMPLATE_LISTING);
-    const tableID = build(dialogID, ids.METADATA_TEMPLATE_LISTING);
+    const dialogID = build(baseId, ids.DIALOG);
+    const listingID = build(dialogID, ids.METADATA_TEMPLATE_LISTING);
 
     if (fetchError) {
         return (
-            <WrappedErrorHandler errorObject={fetchError} baseId={tableID} />
+            <WrappedErrorHandler errorObject={fetchError} baseId={listingID} />
         );
     }
-
-    // Only sort the Templates if sorting has been requested.
-    const sortedTemplates = orderBy
-        ? stableSort(templates, getSorting(order, orderBy))
-        : templates;
-
-    const columnData = getColumnData(t);
 
     return (
         <DEDialog
@@ -214,58 +132,24 @@ const MetadataTemplateListing = (props) => {
                 </Button>,
             ]}
         >
-            <TableContainer component={Paper}>
-                <Table>
-                    {isFetching ? (
-                        <TableLoading
-                            baseId={tableID}
-                            numColumns={columnData?.length}
-                            numRows={5}
-                        />
-                    ) : (
-                        <TableBody>
-                            {sortedTemplates?.map((template) => (
-                                <TemplateTableRow
-                                    key={template.id}
-                                    baseId={build(tableID, template.id)}
-                                    template={template}
-                                    selected={
-                                        selectedTemplateId === template.id
-                                    }
-                                    onClick={() =>
-                                        setSelectedTemplateId(template.id)
-                                    }
-                                    onInfo={(event) => {
-                                        if (template.description) {
-                                            setTemplateInfo(
-                                                template.description
-                                            );
-                                            setTemplateInfoAnchor(
-                                                event.currentTarget
-                                            );
-                                        }
-                                    }}
-                                    onDownload={() =>
-                                        setDownloadTemplateId(template.id)
-                                    }
-                                />
-                            ))}
-                        </TableBody>
-                    )}
-                    <EnhancedTableHead
-                        baseId={tableID}
-                        columnData={columnData}
-                        order={order}
-                        orderBy={orderBy}
-                        onRequestSort={handleRequestSort}
-                    />
-                </Table>
-            </TableContainer>
-            <TemplateInfoDisplay
-                templateInfo={templateInfo}
-                anchorEl={templateInfoAnchor}
-                onClose={() => setTemplateInfoAnchor(null)}
-            />
+            <List>
+                {isFetching
+                    ? [1, 2, 3, 4, 5].map((rowNum) => (
+                          <Skeleton key={rowNum} width="100%" height={100} />
+                      ))
+                    : templates?.map((template) => (
+                          <TemplateListItem
+                              key={template.id}
+                              baseId={build(listingID, template.id)}
+                              template={template}
+                              selected={selectedTemplateId === template.id}
+                              onClick={() => setSelectedTemplateId(template.id)}
+                              onDownload={() =>
+                                  setDownloadTemplateId(template.id)
+                              }
+                          />
+                      ))}
+            </List>
         </DEDialog>
     );
 };

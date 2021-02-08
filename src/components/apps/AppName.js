7c16924f3df5d9b127fce44a12e9e01b38b84237
Sriram Sirinvasan <sriram@cyverse.org>
2021-02-08 15:37:21 -0500

    Display VICE job launch related error.

--- src/components/apps/AppName.js
+++ src/components/apps/AppName.js
@@ -5,7 +5,9 @@
  */
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "i18n";
+import { Trans } from "react-i18next";
 import PropTypes from "prop-types";
+import { intercomShow } from "common/intercom";
 
 import Link from "next/link";
 import DELink from "components/utils/DELink";
@@ -18,18 +20,66 @@ import { Button, Link as MuiLink, Typography } from "@material-ui/core";
 import DEDialog from "components/utils/DEDialog";
 
 function RunErrorDialog(props) {
-    const { code, open } = props;
+    const { baseId, code, open, onClose } = props;
     const { t } = useTranslation("launch");
+    const { t: i18Common } = useTranslation("common");
     let title, msg;
     if (code === ERROR_CODES.ERR_LIMIT_REACHED) {
-        title = "Job Limit Reached";
-        msg = "Already running 2 jobs!";
+        title = t("jobLimitReached");
+        msg = (
+            <Trans
+                t={t}
+                i18nKey="jobLimitReachedPrompt"
+                components={{
+                    b: <b />,
+                    support: (
+                        <MuiLink
+                            href="#"
+                            component="button"
+                            onClick={(event) => {
+                                // prevent form submission
+                                event.preventDefault();
+                                intercomShow();
+                            }}
+                        />
+                    ),
+                }}
+            />
+        );
     } else if (code === ERROR_CODES.ERR_FORBIDDEN) {
-        title = "Access Denied";
-        msg = "You are not allowed to run this app.";
+        title = t("accessDenied");
+        msg = (
+            <Trans
+                t={t}
+                i18nKey="launchForbiddenPrompt"
+                components={{
+                    support: (
+                        <MuiLink
+                            href="#"
+                            component="button"
+                            onClick={(event) => {
+                                // prevent form submission
+                                event.preventDefault();
+                                intercomShow();
+                            }}
+                        />
+                    ),
+                }}
+            />
+        );
     }
     return (
-        <DEDialog title={title} open={open} actions={<Button>OK</Button>}>
+        <DEDialog
+            baseId={baseId}
+            title={title}
+            open={open}
+            actions={
+                <Button color="primary" onClick={onClose}>
+                    {i18Common("ok")}
+                </Button>
+            }
+            onClose={onClose}
+        >
             <Typography>{msg}</Typography>
         </DEDialog>
     );
@@ -49,6 +99,7 @@ function AppName(props) {
     const [accessRequestDialogOpen, setAccessRequestDialogOpen] = useState(
         false
     );
+    const [errorDialogOpen, setErrorDialogOpen] = useState(false);
     const [href, as] = useAppLaunchLink(systemId, appId);
     const { t } = useTranslation("apps");
 
@@ -65,7 +116,7 @@ function AppName(props) {
         title = t("useAppTooltip");
     }
 
-    if (!isDisabled && runErrorCodes?.canRun) {
+    if (!isDisabled && limitChecks?.canRun) {
         return (
             <Link href={href} as={as} passHref>
                 <DELink
@@ -104,12 +155,17 @@ function AppName(props) {
                     <MuiLink
                         component="button"
                         href="#"
-                        onClick={() => setAccessRequestDialogOpen(true)}
+                        onClick={() => setErrorDialogOpen(true)}
                         color="primary"
                     >
                         {name}
                     </MuiLink>
-                    <RunErrorDialog />
+                    <RunErrorDialog
+                        open={errorDialogOpen}
+                        code={runErrorCodes[0]}
+                        onClose={() => setErrorDialogOpen(false)}
+                        baseId={baseDebugId}
+                    />
                 </>
             );
         }

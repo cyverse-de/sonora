68119fb6670f257f650cb0fec40ea0d2414b87fa
Sriram Srinivasan <sriram@cyverse.org>
2021-03-11 20:19:27 +0530

    Add end-points to list and update vice access requests.

--- src/server/api/vice.js
+++ src/server/api/vice.js
@@ -191,5 +191,28 @@ export default function viceRouter() {
         })
     );
 
+    logger.info("adding the GET /admin/requests");
+    api.get(
+        "/admin/requests",
+        auth.authnTokenMiddleware,
+        terrainHandler({
+            method: "GET",
+            pathname: "/admin/requests",
+        })
+    );
+
+    logger.info("adding the POST /admin/requests/:id/:status");
+    api.post(
+        "/admin/requests/:id/:status",
+        auth.authnTokenMiddleware,
+        terrainHandler({
+            method: "POST",
+            pathname: "/admin/requests/:id/:status",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+    );
+
     return api;
 }
--- src/serviceFacades/vice/accessRequest.js
+++ src/serviceFacades/vice/accessRequest.js
@@ -1,9 +1,37 @@
 import callApi from "common/callApi";
-
-export default function requestAccess(request) {
+const ADMIN_ACCESS_REQUEST_LISTING_QUERY_KEY = "fetchAccessRequestListing";
+const ACCESS_REQUEST_IN_PROGRESS = "in-progress";
+const ACCESS_REQUEST_REJECTED = "rejected";
+const ACCESS_REQUEST_APPROVED = "approved";
+function requestAccess(request) {
     return callApi({
         endpoint: "/api/requests/vice",
         method: "POST",
         body: request,
     });
 }
+
+function adminRequestListing() {
+    return callApi({
+        endpoint:
+            "/api/admin/requests?include-completed=true&request-type=vice",
+        method: "GET",
+    });
+}
+
+function adminUpdateRequestStatus(id, status) {
+    return callApi({
+        endpoint: `/api/admin/requests/${id}/${status}`,
+        method: "POST",
+    });
+}
+
+export {
+    requestAccess,
+    adminRequestListing,
+    adminUpdateRequestStatus,
+    ADMIN_ACCESS_REQUEST_LISTING_QUERY_KEY,
+    ACCESS_REQUEST_IN_PROGRESS,
+    ACCESS_REQUEST_REJECTED,
+    ACCESS_REQUEST_APPROVED,
+};

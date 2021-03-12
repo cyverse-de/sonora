8e5d8d03b38b8b8011a04489e41292bce9eed0cb
Sriram Srinivasan <sriram@cyverse.org>
2021-03-12 13:09:54 +0530

    Add logic to update request.

--- src/components/vice/admin/accessRequests/Listing.js
+++ src/components/vice/admin/accessRequests/Listing.js
@@ -6,35 +6,77 @@
  */
 
 import React from "react";
-import { useQuery } from "react-query";
+import { useQuery, queryCache, useMutation } from "react-query";
 import { useTranslation } from "i18n";
+
+import { AnnouncerConstants, announce, build } from "@cyverse-de/ui-lib";
+
 import {
     adminRequestListing,
+    adminUpdateRequestStatus,
     ADMIN_ACCESS_REQUEST_LISTING_QUERY_KEY,
 } from "serviceFacades/vice/accessRequest";
+
 import TableView from "./TableView";
 import TableLoading from "components/utils/TableLoading";
 import ErrorHandler from "components/utils/error/ErrorHandler";
+import withErrorAnnouncer from "components/utils/error/withErrorAnnouncer";
 
-export default function Listing(props) {
-    const { baseId } = props;
+function Listing(props) {
+    const { baseId, showErrorAnnouncer } = props;
     const { t } = useTranslation("vice-admin");
     const [data, setData] = React.useState(null);
+    const [showAllRequest, setShowAllRequest] = React.useState(false);
+
+    const handleRequestFilterChange = (event) => {
+        setShowAllRequest(event.target.checked);
+    };
 
     const { isFetching, error } = useQuery({
-        queryKey: [ADMIN_ACCESS_REQUEST_LISTING_QUERY_KEY],
+        queryKey: [ADMIN_ACCESS_REQUEST_LISTING_QUERY_KEY, { showAllRequest }],
         queryFn: adminRequestListing,
         config: {
             enabled: true,
             onSuccess: setData,
         },
     });
+
+    const [updateRequest, { isLoading: updateLoading }] = useMutation(
+        adminUpdateRequestStatus,
+        {
+            onSuccess: (resp) => {
+                announce({
+                    text: t("accessRequestUpdateSuccess"),
+                    variant: AnnouncerConstants.SUCCESS,
+                });
+                queryCache.invalidateQueries(
+                    ADMIN_ACCESS_REQUEST_LISTING_QUERY_KEY
+                );
+            },
+            onError: (error) => {
+                showErrorAnnouncer(t("accessRequestUpdateFailure"), error);
+            },
+        }
+    );
+
+    const onUpdateRequest = (id, status, message) => {
+        updateRequest({ id, status, message });
+    };
+
     if (error) {
         return <ErrorHandler errorObject={error} />;
     }
-    if (isFetching || !data) {
-        return <TableLoading rows={10} />;
+    if (isFetching || !data || updateLoading) {
+        return <TableLoading rows={10} numColumns={5} />;
     } else {
-        return <TableView data={data?.requests} />;
+        return (
+            <TableView
+                data={data?.requests}
+                onUpdateRequest={onUpdateRequest}
+                showAllRequest={showAllRequest}
+                onRequestFilterChange={handleRequestFilterChange}
+            />
+        );
     }
 }
+export default withErrorAnnouncer(Listing);
--- src/components/vice/admin/accessRequests/TableView.js
+++ src/components/vice/admin/accessRequests/TableView.js
@@ -6,14 +6,29 @@
  *
  */
 import React, { useMemo } from "react";
+import { useTranslation } from "i18n";
 import { build } from "@cyverse-de/ui-lib";
+import {
+    ACCESS_REQUEST_REJECTED,
+    ACCESS_REQUEST_APPROVED,
+    ACCESS_REQUEST_COMPLETED,
+} from "serviceFacades/vice/accessRequest";
 import BasicTable from "components/utils/BasicTable";
 import { IconButton, Paper, Typography } from "@material-ui/core";
 import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
 export default function TableView(props) {
-    const { baseId, data } = props;
+    const {
+        baseId,
+        data,
+        onUpdateRequest,
+        showAllRequest,
+        onRequestFilterChange,
+    } = props;
+    const { t } = useTranslation("vice-admin");
     const columns = useMemo(
         () => [
             {
@@ -48,8 +63,23 @@ export default function TableView(props) {
                 Header: "Approve",
                 id: "approveButton",
                 Cell: ({ row, value }) => {
+                    const original = row.original;
                     return (
-                        <IconButton>
+                        <IconButton
+                            color={
+                                original?.status?.toLowerCase() ===
+                                ACCESS_REQUEST_COMPLETED
+                                    ? "primary"
+                                    : "default"
+                            }
+                            onClick={() =>
+                                onUpdateRequest(
+                                    original.id,
+                                    ACCESS_REQUEST_APPROVED,
+                                    t("accessRequestApprovedMsg")
+                                )
+                            }
+                        >
                             <ThumbUpIcon />
                         </IconButton>
                     );
@@ -59,8 +89,23 @@ export default function TableView(props) {
                 Header: "Reject",
                 id: "rejectButton",
                 Cell: ({ row, value }) => {
+                    const original = row.original;
                     return (
-                        <IconButton>
+                        <IconButton
+                            color={
+                                original?.status?.toLowerCase() ===
+                                ACCESS_REQUEST_REJECTED
+                                    ? "primary"
+                                    : "default"
+                            }
+                            onClick={() =>
+                                onUpdateRequest(
+                                    original.id,
+                                    ACCESS_REQUEST_REJECTED,
+                                    t("accessRequestDeniedMsg")
+                                )
+                            }
+                        >
                             <ThumbDownIcon />
                         </IconButton>
                     );
@@ -71,9 +116,23 @@ export default function TableView(props) {
     );
     return (
         <Paper style={{ marginTop: 16, marginBottom: 16 }}>
-            <Typography variant="h6" style={{ padding: 16 }}>
-                Access Requests
-            </Typography>
+            <div style={{ padding: 16 }}>
+                <Typography variant="h6" component="span">
+                    Access Requests
+                </Typography>
+                <FormControlLabel
+                    style={{ float: "right" }}
+                    control={
+                        <Switch
+                            checked={showAllRequest}
+                            onChange={onRequestFilterChange}
+                            name="requestFilter"
+                            color="primary"
+                        />
+                    }
+                    label="Show All Requests"
+                />
+            </div>
             <BasicTable columns={columns} data={data} />
         </Paper>
     );

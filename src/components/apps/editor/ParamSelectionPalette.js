315dc8b6dc73e94b1ca44fc5c92580e953e83624
Paul Sarando <psarando@iplantcollaborative.org>
2021-03-24 14:01:12 -0700

    CORE-1320 Add List item icons to App Param Selection Palette

--- src/components/apps/editor/ParamSelectionPalette.js
+++ src/components/apps/editor/ParamSelectionPalette.js
@@ -9,22 +9,111 @@ import { Trans } from "react-i18next";
 
 import { useTranslation } from "i18n";
 
+import ResourceIcon from "components/data/listing/ResourceIcon";
+
 import AppParamTypes from "components/models/AppParamTypes";
+import ResourceTypes from "components/models/ResourceTypes";
 import DEDialog from "components/utils/DEDialog";
 
 import { build as buildID } from "@cyverse-de/ui-lib";
 
 import {
+    Avatar,
     Collapse,
     List,
     ListItem,
+    ListItemAvatar,
     ListItemText,
     ListItemSecondaryAction,
     ListSubheader,
     IconButton,
+    Typography,
 } from "@material-ui/core";
 
-import HelpIcon from "@material-ui/icons/Help";
+import {
+    CheckBox,
+    Code,
+    FileCopy as MultiFileIcon,
+    Help as HelpIcon,
+    Info as InfoIcon,
+    List as ListIcon,
+    Subject as MultiLineTextIcon,
+    TextFields as TextIcon,
+} from "@material-ui/icons";
+
+function ParamTypeIcon(props) {
+    const { paramType } = props;
+
+    let icon;
+
+    switch (paramType) {
+        case AppParamTypes.FILE_INPUT:
+        case AppParamTypes.FILE_OUTPUT:
+            icon = <ResourceIcon type={ResourceTypes.FILE} />;
+            break;
+
+        case AppParamTypes.FOLDER_INPUT:
+        case AppParamTypes.FOLDER_OUTPUT:
+            icon = <ResourceIcon type={ResourceTypes.FOLDER} />;
+            break;
+
+        case AppParamTypes.MULTIFILE_SELECTOR:
+        case AppParamTypes.MULTIFILE_OUTPUT:
+            icon = <MultiFileIcon color="action" />;
+            break;
+
+        case AppParamTypes.INFO:
+            icon = <InfoIcon color="action" />;
+            break;
+
+        case AppParamTypes.TEXT:
+            icon = <TextIcon color="action" />;
+            break;
+
+        case AppParamTypes.MULTILINE_TEXT:
+            icon = <MultiLineTextIcon color="action" />;
+            break;
+
+        case AppParamTypes.FLAG:
+            icon = <CheckBox color="action" />;
+            break;
+
+        case AppParamTypes.INTEGER:
+            icon = (
+                <Typography variant="button" color="textSecondary">
+                    123
+                </Typography>
+            );
+            break;
+
+        case AppParamTypes.DOUBLE:
+            icon = (
+                <Typography variant="button" color="textSecondary">
+                    2.3
+                </Typography>
+            );
+            break;
+
+        case AppParamTypes.TEXT_SELECTION:
+        case AppParamTypes.INTEGER_SELECTION:
+        case AppParamTypes.DOUBLE_SELECTION:
+        case AppParamTypes.REFERENCE_GENOME:
+        case AppParamTypes.REFERENCE_SEQUENCE:
+        case AppParamTypes.REFERENCE_ANNOTATION:
+            icon = <ListIcon color="action" />;
+            break;
+
+        default:
+            icon = <Code color="action" />;
+            break;
+    }
+
+    return (
+        <ListItemAvatar>
+            <Avatar>{icon}</Avatar>
+        </ListItemAvatar>
+    );
+}
 
 function ParamTypeListItem(props) {
     const { id, paramType, onClick } = props;
@@ -40,6 +129,7 @@ function ParamTypeListItem(props) {
     return (
         <>
             <ListItem id={id} divider={!helpOpen} button onClick={onClick}>
+                <ParamTypeIcon paramType={paramType} />
                 <ListItemText primary={t(paramType)} />
                 <ListItemSecondaryAction>
                     <IconButton

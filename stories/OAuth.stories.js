7c725aef4af7ee308eb9348397bcbb353362d4dc
Sarah Roberts <sarahr@cyverse.org>
2021-03-25 14:01:45 -0700

    CORE-1282: added stories for the rest of the OAuth error codes

--- stories/OAuth.stories.js
+++ stories/OAuth.stories.js
@@ -3,6 +3,60 @@ import OAuthErrorHandler from "../src/components/oauth/OAuthErrorHandler";
 
 export default { title: "OAuth" };
 
-export const InvalidRequest = () => (
-    <OAuthErrorHandler errorCode="invalid_request" baseId="someBaseId" />
-);
+const Template = (args) => <OAuthErrorHandler baseId="someBaseId" {...args} />;
+
+export const InvalidRequest = Template.bind({});
+InvalidRequest.args = {
+    errorCode: "invalid_request",
+};
+
+export const UnauthorizedClient = Template.bind({});
+UnauthorizedClient.args = {
+    errorCode: "unauthorized_client",
+};
+
+export const AccessDenied = Template.bind({});
+AccessDenied.args = {
+    errorCode: "access_denied",
+};
+
+export const UnsupportedResponseType = Template.bind({});
+UnsupportedResponseType.args = {
+    errorCode: "unsupported_response_type",
+};
+
+export const InvalidScope = Template.bind({});
+InvalidScope.args = {
+    errorCode: "invalid_scope",
+};
+
+export const ServerError = Template.bind({});
+ServerError.args = {
+    errorCode: "server_error",
+};
+
+export const TemporarilyUnavailable = Template.bind({});
+TemporarilyUnavailable.args = {
+    errorCode: "temporarily_unavailable",
+};
+
+export const InvalidOAuthConfig = Template.bind({});
+InvalidOAuthConfig.args = {
+    errorCode: "invalid_oauth_config",
+};
+InvalidOAuthConfig.storyName = "Invalid OAuth Config";
+
+export const NoAuthCodeProvided = Template.bind({});
+NoAuthCodeProvided.args = {
+    errorCode: "no_auth_code_provided",
+};
+
+export const NoStateIdProvided = Template.bind({});
+NoStateIdProvided.args = {
+    errorCode: "no_state_id_provided",
+};
+
+export const GeneralServiceError = Template.bind({});
+GeneralServiceError.args = {
+    errorCode: "general_service_error",
+};

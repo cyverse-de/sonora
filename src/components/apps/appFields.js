a0aba9c991128b74501dcad45f7e70efe9eca540
Sriram Sirinvasan <sriram@cyverse.org>
2021-03-29 14:45:41 -0400

    core-1357 Fix rating key.

--- src/components/apps/appFields.js
+++ src/components/apps/appFields.js
@@ -1,7 +1,7 @@
 const appFields = (appsI18n) => ({
     NAME: { fieldName: appsI18n("name"), key: "name" },
     INTEGRATOR: { fieldName: appsI18n("integratedBy"), key: "integrator_name" },
-    RATING: { fieldName: appsI18n("rating"), key: "average_rating" },
+    RATING: { fieldName: appsI18n("rating"), key: "average" },
     SYSTEM: { fieldName: appsI18n("systemId"), key: "system_id" },
     STATUS: { fieldName: "", key: "status" },
     DOT_MENU: { fieldName: "", key: "dotMenu" },

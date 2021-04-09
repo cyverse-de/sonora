2ef44e4d8c755fde539f44ec090901640575626b
Ashley Ramsey <aramsey@cyverse.org>
2021-04-09 16:05:49 -0700

    CORE-1298 Add component for VICE urls in notification menu

---
+++ src/components/notifications/RunningViceTab.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+import { build } from "@cyverse-de/ui-lib";
+import { List, ListItem, Typography } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
+
+import { openInteractiveUrl } from "components/analyses/utils";
+import DELink from "components/utils/DELink";
+import { useUserProfile } from "contexts/userProfile";
+import { useTranslation } from "i18n";
+import ids from "./ids";
+import { useRunningViceJobs } from "serviceFacades/analyses";
+
+function RunningViceTab(props) {
+    const { baseId, handleClose } = props;
+    const { t } = useTranslation("common");
+    const [analyses, setAnalyses] = useState([]);
+    const [userProfile] = useUserProfile();
+
+    const { isFetching } = useRunningViceJobs({
+        enabled: userProfile?.id,
+        onSuccess: (resp) => {
+            const analyses = resp?.analyses?.filter(
+                (analysis) => analysis?.interactive_urls?.length > 0
+            );
+            setAnalyses(analyses);
+        },
+    });
+
+    if (isFetching) {
+        return (
+            <div id={build(baseId, ids.LOADING_SKELETON)}>
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <Typography variant="h5">{t("runningVice")}</Typography>
+            <List component="div">
+                {analyses?.map((analysis) => (
+                    <ListItem>
+                        <DELink
+                            onClick={() => {
+                                const accessUrl = analysis.interactive_urls[0];
+                                openInteractiveUrl(accessUrl);
+                                handleClose();
+                            }}
+                            id={build(baseId, analysis.id)}
+                            key={analysis.id}
+                            text={analysis.name}
+                        />
+                    </ListItem>
+                ))}
+            </List>
+        </>
+    );
+}
+
+export default RunningViceTab;

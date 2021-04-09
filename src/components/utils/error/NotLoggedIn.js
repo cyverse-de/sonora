718a6b51757bec194625b51553cd473c38562baa
flynn <abhyudaygaur@email.arizona.edu>
2021-04-09 11:36:21 -0700

    Use constant for error 401

--- src/components/utils/error/NotLoggedIn.js
+++ src/components/utils/error/NotLoggedIn.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ErrorHandler from "components/utils/error/ErrorHandler";
+import { signInErrorResponse } from "./errorCode";
 
 const NotLoggedIn = (props) => {
-    return (
-        <ErrorHandler errorObject={{ response: { status: 401 } }} {...props} />
-    );
+    return <ErrorHandler errorObject={signInErrorResponse} {...props} />;
 };
 
 export default NotLoggedIn;

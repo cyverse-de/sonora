f2f8e53f4239c37683e7ab5a9c79da53269aab27
Sriram Sirinvasan <sriram@cyverse.org>
2021-04-20 14:30:21 -0400

    Implement remember last folder preference.

--- src/components/apps/launch/params/FolderInput.js
+++ src/components/apps/launch/params/FolderInput.js
@@ -9,7 +9,11 @@ import InputSelector from "../InputSelector";
 
 import ResourceTypes from "components/models/ResourceTypes";
 
+import { useBootstrapInfo } from "contexts/bootstrap";
+
 export default function FileFolderInput({ param, ...props }) {
+    const bootstrapInfo = useBootstrapInfo()[0];
+    const pref = bootstrapInfo?.preferences;
     return (
         <InputSelector
             margin="normal"
@@ -17,6 +21,11 @@ export default function FileFolderInput({ param, ...props }) {
             label={param?.label}
             helperText={param?.description}
             required={param?.required}
+            startingPath={
+                pref?.rememberLastPath
+                    ? pref?.lastFolder
+                    : pref?.defaultFileSelectorPath
+            }
             {...props}
         />
     );

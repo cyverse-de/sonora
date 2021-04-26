a1701756695e4e447889c34bd549a4eb9b2676cf
John Wregglesworth <wregglej@cyverse.org>
2021-04-26 11:49:46 -0700

    Get the quick launch list story working again

--- stories/instantlaunches/admin/QuickLaunchList.stories.js
+++ stories/instantlaunches/admin/QuickLaunchList.stories.js
@@ -18,9 +18,13 @@ export const QuickLaunchListTest = () => {
     mockAxios
         .onGet("/api/quicklaunches/defaults/global")
         .reply(200, testGlobalQLs);
-    mockAxios.onGet("/api/instantlaunches").reply(200, testInstantLaunches);
+    mockAxios
+        .onGet("/api/instantlaunches/full")
+        .reply(200, testInstantLaunches);
     mockAxios.onPost("/api/quicklaunches/defaults/global").reply(200, {});
-    mockAxios.onGet("/api/quicklaunches").reply(200, testData);
+    mockAxios
+        .onGet("/api/instantlaunches/quicklaunches/public")
+        .reply(200, testData);
 
     mockAxios.onPut("/api/instantlaunches").reply(200, {});
 

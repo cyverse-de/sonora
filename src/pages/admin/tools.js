4cca500fa8c0eb1bfebb76aea883d19ce5f2161f
Sriram Sirinvasan <sriram@cyverse.org>
2021-07-09 15:28:45 -0400

    core-1459 Refactor tools admin page to use tabs.

--- src/pages/admin/tools.js
+++ src/pages/admin/tools.js
@@ -8,41 +8,83 @@
 
 import React, { useCallback } from "react";
 import { useRouter } from "next/router";
+import { useTranslation } from "i18n";
 
 import constants from "../../constants";
 import { getLocalStorage } from "components/utils/localStorage";
 import Listing from "components/tools/listing/Listing";
+import RequestListing from "components/tools/requests/Listing";
 
+import { DETabs, DETab, DETabPanel } from "components/utils/DETabs";
 import NavigationConstants from "common/NavigationConstants";
 import { useUserProfile } from "contexts/userProfile";
 import NotAuthorized from "components/utils/error/NotAuthorized";
 
+const TABS = {
+    listing: "LISTING",
+    toolRequests: "TOOL REQUESTS",
+};
+
 export default function Tools() {
     const router = useRouter();
+    const { t } = useTranslation("tools");
     const query = router.query;
-    const selectedPage = parseInt(query.selectedPage) || 0;
-    const selectedRowsPerPage =
-        parseInt(query.selectedRowsPerPage) ||
+    const selectedToolsPage = parseInt(query.selectedToolsPage) || 0;
+    const selectedToolsRowsPerPage =
+        parseInt(query.selectedToolsRowsPerPage) ||
         parseInt(getLocalStorage(constants.LOCAL_STORAGE.TOOLS.PAGE_SIZE)) ||
         100;
-    const selectedOrder = query.selectedOrder || constants.SORT_ASCENDING;
-    const selectedOrderBy = query.selectedOrderBy || "name";
-    const selectedPermFilter = query.selectedPermFilter;
-    const selectedSearchTerm = query.selectedSearchTerm || "";
+    const selectedToolsOrder =
+        query.selectedToolsOrder || constants.SORT_ASCENDING;
+    const selectedToolsOrderBy = query.selectedToolsOrderBy || "name";
+    const selectedToolsPermFilter = query.selectedToolsPermFilter;
+    const selectedToolsSearchTerm = query.selectedToolsSearchTerm || "";
+
+    const selectedRequestsPage = parseInt(query.selectedRequestsPage) || 0;
+    const selectedRequestsRowsPerPage =
+        parseInt(query.selectedRequestsRowsPerPage) ||
+        parseInt(getLocalStorage(constants.LOCAL_STORAGE.TOOLS.PAGE_SIZE)) ||
+        100;
+    const selectedRequestsOrder =
+        query.selectedRequestsOrder || constants.SORT_DESCENDING;
+    const selectedRequestsOrderBy =
+        query.selectedRequestsOrderBy || "date_submitted";
+
     const profile = useUserProfile()[0];
     const isAdmin = profile?.admin;
 
-    const onRouteToListing = useCallback(
+    const [selectedTab, setSelectedTab] = React.useState(TABS.listing);
+
+    const onTabSelectionChange = (event, selectedTab) => {
+        setSelectedTab(selectedTab);
+    };
+
+    const onRouteToToolsListing = useCallback(
         (order, orderBy, page, rowsPerPage, permFilter, searchTerm) => {
             router.push({
                 pathname: `/${NavigationConstants.ADMIN}/${NavigationConstants.TOOLS}`,
                 query: {
-                    selectedOrder: order,
-                    selectedOrderBy: orderBy,
-                    selectedPage: page,
-                    selectedRowsPerPage: rowsPerPage,
-                    selectedPermFilter: permFilter,
-                    selectedSearchTerm: searchTerm,
+                    selectedToolsOrder: order,
+                    selectedToolsOrderBy: orderBy,
+                    selectedToolsPage: page,
+                    selectedToolsRowsPerPage: rowsPerPage,
+                    selectedToolsPermFilter: permFilter,
+                    selectedToolsSearchTerm: searchTerm,
+                },
+            });
+        },
+        [router]
+    );
+
+    const onRouteToRequestsListing = useCallback(
+        (order, orderBy, page, rowsPerPage) => {
+            router.push({
+                pathname: `/${NavigationConstants.ADMIN}/${NavigationConstants.TOOLS}`,
+                query: {
+                    selectedRequestsOrder: order,
+                    selectedRequestsOrderBy: orderBy,
+                    selectedRequestsPage: page,
+                    selectedRequestsRowsPerPage: rowsPerPage,
                 },
             });
         },
@@ -53,17 +95,51 @@ export default function Tools() {
         return <NotAuthorized />;
     } else {
         return (
-            <Listing
-                baseId="adminTools"
-                page={selectedPage}
-                rowsPerPage={selectedRowsPerPage}
-                order={selectedOrder}
-                orderBy={selectedOrderBy}
-                permFilter={selectedPermFilter}
-                onRouteToListing={onRouteToListing}
-                searchTerm={selectedSearchTerm}
-                isAdmin={isAdmin}
-            />
+            <>
+                <DETabs value={selectedTab} onChange={onTabSelectionChange}>
+                    <DETab
+                        value={TABS.listing}
+                        label={t("listing")}
+                        id="toolListing"
+                    />
+                    <DETab
+                        value={TABS.toolRequests}
+                        label={t("toolRequests")}
+                        id="toolRequests"
+                    />
+                </DETabs>
+                <DETabPanel
+                    tabId="appListing"
+                    value={TABS.listing}
+                    selectedTab={selectedTab}
+                >
+                    <Listing
+                        baseId="adminTools"
+                        page={selectedToolsPage}
+                        rowsPerPage={selectedToolsRowsPerPage}
+                        order={selectedToolsOrder}
+                        orderBy={selectedToolsOrderBy}
+                        permFilter={selectedToolsPermFilter}
+                        onRouteToListing={onRouteToToolsListing}
+                        searchTerm={selectedToolsSearchTerm}
+                        isAdmin={isAdmin}
+                    />
+                </DETabPanel>
+                <DETabPanel
+                    tabId="toolRequestsListing"
+                    value={TABS.toolRequests}
+                    selectedTab={selectedTab}
+                >
+                    <RequestListing
+                        baseId="adminToolRequests"
+                        page={selectedRequestsPage}
+                        rowsPerPage={selectedRequestsRowsPerPage}
+                        order={selectedRequestsOrder}
+                        orderBy={selectedRequestsOrderBy}
+                        onRouteToRequestsListing={onRouteToRequestsListing}
+                    />
+                </DETabPanel>
+            </>
         );
     }
 }

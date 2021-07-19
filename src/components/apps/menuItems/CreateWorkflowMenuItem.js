2e338b86e7e96fd41e0a8bca70536ddf5dd01e8a
Sriram Sirinvasan <sriram@cyverse.org>
2021-07-19 14:24:26 -0400

    Fix rebase issues.

--- src/components/apps/menuItems/CreateWorkflowMenuItem.js
+++ src/components/apps/menuItems/CreateWorkflowMenuItem.js
@@ -9,7 +9,7 @@ import { useTranslation } from "i18n";
 import ids from "../ids";
 import NavigationConstants from "common/NavigationConstants";
 
-import { build } from "@cyverse-de/ui-lib";
+import buildID from "components/utils/DebugIDUtil";
 
 import { ListItemIcon, ListItemText, MenuItem } from "@material-ui/core";
 import { Add as CreateAppIcon } from "@material-ui/icons";
@@ -23,7 +23,7 @@ const CreateWorkflowMenuItem = React.forwardRef((props, ref) => {
         <Link href={NavigationConstants.NEW_WORKFLOW}>
             <MenuItem
                 ref={ref}
-                id={build(baseId, ids.CREATE_WORKFLOW_MENU_ITEM)}
+                id={buildID(baseId, ids.CREATE_WORKFLOW_MENU_ITEM)}
             >
                 <ListItemIcon>
                     <CreateAppIcon fontSize="small" />

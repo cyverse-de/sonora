e0cf060b6a7d45e38c991b9d53570779498752b5
Sriram Sirinvasan <sriram@cyverse.org>
2021-07-27 14:48:28 -0400

    core-1463 Fix free solo value selection and other minor cosmetic issues.

--- src/components/forms/FormSearchField.js
+++ src/components/forms/FormSearchField.js
@@ -2,14 +2,14 @@
  * @author psarando sriram
  */
 import React from "react";
-
-import Autocomplete from "@material-ui/lab/Autocomplete";
+import { useTranslation } from "i18n";
 
 import getFormError from "./getFormError";
 
-import FormControl from "@material-ui/core/FormControl";
+import Autocomplete, {
+    createFilterOptions,
+} from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
 
 const FormSearchField = ({
     field: { value, onBlur, onChange, ...field },
@@ -25,6 +25,8 @@ const FormSearchField = ({
 }) => {
     const errorMsg = getFormError(field.name, touched, errors);
     const [searchValue, setSearchValue] = React.useState(value);
+    const filter = createFilterOptions();
+    const { t } = useTranslation("common");
 
     React.useEffect(() => {
         const val = {};
@@ -32,39 +34,63 @@ const FormSearchField = ({
         setSearchValue(val);
     }, [value, setSearchValue, props.labelKey]);
 
-    const onOptionSelected = (event, option, reason) => {
-        setFieldValue(field.name, option ? option[props.valueKey] : "");
+    const onOptionSelected = (event, newValue) => {
+        if (newValue) {
+            if (typeof newValue === "string") {
+                setFieldValue(field.name, newValue);
+            } else if (newValue && newValue.inputValue) {
+                // Create a new value from the user input
+                setFieldValue(field.name, newValue.inputValue);
+            } else {
+                setFieldValue(field.name, newValue[props.valueKey]);
+            }
+        } else {
+            setFieldValue(field.name, "");
+        }
     };
+
     return (
-        <FormControl fullWidth error={!!errorMsg}>
-            <Autocomplete
-                id="searchField"
-                getOptionSelected={(option, value) =>
-                    option[props.labelKey] === value[props.labelKey]
+        <Autocomplete
+            id="searchField"
+            getOptionSelected={(option, value) =>
+                option[props.labelKey] === value[props.labelKey]
+            }
+            getOptionLabel={(option) => (option ? option[props.labelKey] : "")}
+            options={options}
+            size={size}
+            freeSolo={true}
+            selectOnFocus={true}
+            value={searchValue}
+            handleHomeEndKeys={true}
+            onInputChange={handleSearch}
+            onChange={onOptionSelected}
+            renderOption={(option, state) => renderCustomOption(option)}
+            filterOptions={(options, params) => {
+                const filtered = filter(options, params);
+                // Suggest the creation of a new value
+                if (params.inputValue !== "") {
+                    filtered.push({
+                        inputValue: params.inputValue,
+                        label: t("formatTermFreeTextOption", {
+                            inputValue: params.inputValue,
+                        }),
+                    });
                 }
-                getOptionLabel={(option) =>
-                    option ? option[props.labelKey] : ""
-                }
-                options={options}
-                size={size}
-                freeSolo={true}
-                value={searchValue}
-                onInputChange={handleSearch}
-                onChange={onOptionSelected}
-                renderOption={(option, state) => renderCustomOption(option)}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        label={helperText}
-                        variant="outlined"
-                        InputProps={{
-                            ...params.InputProps,
-                        }}
-                    />
-                )}
-            />
-            <FormHelperText>{errorMsg || helperText}</FormHelperText>
-        </FormControl>
+
+                return filtered;
+            }}
+            renderInput={(params) => (
+                <TextField
+                    error={!!errorMsg}
+                    label={label}
+                    variant="outlined"
+                    helperText={errorMsg || helperText}
+                    {...params}
+                />
+            )}
+            {...field}
+            {...props}
+        />
     );
 };
 

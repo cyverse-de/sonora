568ee6d0310d5dc6d84fac93a0e4833db759e603
Ashley Ramsey <aramsey@cyverse.org>
2021-12-10 09:10:18 -0700

    CORE-1609 Fix metadata styling

--- src/components/search/form/clauses/Metadata.js
+++ src/components/search/form/clauses/Metadata.js
@@ -6,6 +6,7 @@ import { FormTextField } from "components/forms/FormField";
 import buildID from "components/utils/DebugIDUtil";
 import { useTranslation } from "i18n";
 import ids from "../ids";
+import { Grid } from "@material-ui/core";
 
 const METADATA_TYPE = "metadata";
 const METADATA_ARGS_DEFAULT = { attribute: "", value: "" };
@@ -19,22 +20,26 @@ function Metadata(props) {
     const { t } = useTranslation("search");
 
     return (
-        <>
-            <Field
-                name={`${name}.attribute`}
-                label={t("attribute")}
-                id={buildID(parentId, ids.METADATA_ATTRIBUTE)}
-                fullWidth={false}
-                component={FormTextField}
-            />
-            <Field
-                name={`${name}.value`}
-                label={t("value")}
-                id={buildID(parentId, ids.METADATA_VALUE)}
-                fullWidth={false}
-                component={FormTextField}
-            />
-        </>
+        <Grid container spacing={1}>
+            <Grid item>
+                <Field
+                    name={`${name}.attribute`}
+                    label={t("attribute")}
+                    id={buildID(parentId, ids.METADATA_ATTRIBUTE)}
+                    fullWidth={false}
+                    component={FormTextField}
+                />
+            </Grid>
+            <Grid item>
+                <Field
+                    name={`${name}.value`}
+                    label={t("value")}
+                    id={buildID(parentId, ids.METADATA_VALUE)}
+                    fullWidth={false}
+                    component={FormTextField}
+                />
+            </Grid>
+        </Grid>
     );
 }
 

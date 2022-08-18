b2838b95e20efd0303f2c7f399ef9997d8c028db
Sarah Roberts <sarahr@cyverse.org>
2022-08-18 13:50:33 -0700

    CORE-1763: fixed several 404 responses in the VICE admin page storybook

--- stories/vice/admin/VICEAdmin.stories.js
+++ stories/vice/admin/VICEAdmin.stories.js
@@ -4,7 +4,14 @@ import { VICEAdmin } from "pages/admin/vice";
 
 import { mockAxios } from "../../axiosMock";
 
-import { viceResources, viceRequests } from "./mocks";
+import {
+    viceResources,
+    viceRequests,
+    instantLaunches,
+    instantLaunchMetadata,
+    instantLaunchMappings,
+    publicQuicklaunches,
+} from "./mocks";
 
 export default {
     title: "VICE / admin / VICEAdmin",
@@ -15,5 +22,17 @@ export const VICEAdminTest = () => {
         .onGet(/\/api\/admin\/vice\/resources.*/)
         .reply(200, viceResources);
     mockAxios.onGet(/\/api\/admin\/requests.*/).reply(200, viceRequests);
+    mockAxios
+        .onGet(/\/api\/instantlaunches\/full.*/)
+        .reply(200, instantLaunches);
+    mockAxios
+        .onGet(/\/api\/admin\/instantlaunches\/metadata\/full.*/)
+        .reply(200, instantLaunchMetadata);
+    mockAxios
+        .onGet(/\/api\/instantlaunches\/mappings\/defaults\/latest.*/)
+        .reply(200, instantLaunchMappings);
+    mockAxios
+        .onGet(/\/api\/instantlaunches\/quicklaunches\/public.*/)
+        .reply(200, publicQuicklaunches);
     return <VICEAdmin />;
 };

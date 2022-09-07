44d41547deffbf48b6a14a8a64496718adf9c92a
Sarah Roberts <sarahr@cyverse.org>
2022-09-07 14:52:13 -0700

    CORE-1793: implement quota limit enforcement for the instant launch listing

--- stories/instantlaunches/InstantLaunchListing.stories.js
+++ stories/instantlaunches/InstantLaunchListing.stories.js
@@ -3,17 +3,37 @@ import React from "react";
 import { mockAxios } from "../axiosMock";
 import { testFullInstantLaunchList } from "./admin/SavedLaunchListData";
 import InstantLaunchListing from "../../src/components/instantlaunches/listing/index";
+import {
+    usageSummaryResponse,
+    usageSummaryComputeLimitExceededResponse,
+} from "../data/DataMocksInstantLaunch";
 
 export default {
     title: "Instant Launches / Instant Launch Listing",
 };
 
-export const InstantLaunchListTest = () => {
+const InstantLaunchListTestTemplate = (args) => {
+    const { instantLaunchListing, usageSummaryResponse } = args;
     mockAxios
         .onGet("/api/instantlaunches/metadata/full", {
             params: { attribute: "ui_location", value: "listing", unit: "" },
         })
-        .reply(200, testFullInstantLaunchList);
+        .reply(200, instantLaunchListing);
+    mockAxios
+        .onGet(/\/api\/resource-usage\/summary.*/)
+        .reply(200, usageSummaryResponse);
 
     return <InstantLaunchListing />;
+};
+
+export const NormalListing = InstantLaunchListTestTemplate.bind({});
+NormalListing.args = {
+    instantLaunchListing: testFullInstantLaunchList,
+    usageSummaryResponse: usageSummaryResponse,
+};
+
+export const ComputeLimitExceeded = InstantLaunchListTestTemplate.bind({});
+ComputeLimitExceeded.args = {
+    instantLaunchListing: testFullInstantLaunchList,
+    usageSummaryResponse: usageSummaryComputeLimitExceededResponse,
 };

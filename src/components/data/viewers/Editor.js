9d708fefbf435642501fa508f8a5d0ee560cb29c
Paul Sarando <psarando@cyverse.org>
2023-01-12 19:49:19 -0700

    CORE-1867 Allow text wrap and line number toggle in CodeMirror Editor

--- src/components/data/viewers/Editor.js
+++ src/components/data/viewers/Editor.js
@@ -7,8 +7,8 @@
  */
 import React, { useEffect, useRef, useState } from "react";
 
-import { EditorView } from "@codemirror/view";
-import { EditorState } from "@codemirror/state";
+import { EditorView, lineNumbers } from "@codemirror/view";
+import { EditorState, Compartment } from "@codemirror/state";
 import { minimalSetup } from "codemirror";
 
 import buildID from "components/utils/DebugIDUtil";
@@ -22,20 +22,24 @@ export default function Editor(props) {
     const {
         baseId,
         // mode = "",
-        // showLineNumbers,
+        showLineNumbers,
         // editable,
-        // wrapText,
+        wrapText,
         editorValue,
         setEditorValue,
         setDirty,
     } = props;
 
+    const [wrapTextConfig] = useState(new Compartment());
+    const [lineNumbersConfig] = useState(new Compartment());
     const [editorView, setEditorView] = useState();
     const [editorState] = useState(
         EditorState.create({
             doc: editorValue,
             extensions: [
                 minimalSetup,
+                wrapTextConfig.of(wrapText ? EditorView.lineWrapping : []),
+                lineNumbersConfig.of(showLineNumbers ? lineNumbers() : []),
                 EditorView.baseTheme({
                     "&": { height: viewerConstants.DEFAULT_VIEWER_HEIGHT },
                 }),
@@ -61,6 +65,26 @@ export default function Editor(props) {
 
         return () => view.destroy();
     }, [editor, editorState]);
+
+    useEffect(() => {
+        if (editorView) {
+            editorView.dispatch({
+                effects: wrapTextConfig.reconfigure(
+                    wrapText ? EditorView.lineWrapping : []
+                ),
+            });
+        }
+    }, [editorView, wrapText, wrapTextConfig]);
+
+    useEffect(() => {
+        if (editorView) {
+            editorView.dispatch({
+                effects: lineNumbersConfig.reconfigure(
+                    showLineNumbers ? lineNumbers() : []
+                ),
+            });
+        }
+    }, [editorView, lineNumbersConfig, showLineNumbers]);
 
     return (
         <>

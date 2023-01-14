658b8d5b5cd8bc711b84c99e8bb9c11f4ac98623
Paul Sarando <psarando@cyverse.org>
2023-01-13 17:18:40 -0700

    CORE-1867 Add CodeMirror v6 language modes

--- src/components/data/viewers/Editor.js
+++ src/components/data/viewers/Editor.js
@@ -11,17 +11,27 @@ import { EditorView, lineNumbers } from "@codemirror/view";
 import { EditorState, Compartment } from "@codemirror/state";
 import { minimalSetup } from "codemirror";
 
+import { markdown } from "@codemirror/lang-markdown";
+import { python } from "@codemirror/lang-python";
+
+import { StreamLanguage } from "@codemirror/language";
+import { dockerFile } from "@codemirror/legacy-modes/mode/dockerfile";
+import { perl } from "@codemirror/legacy-modes/mode/perl";
+import { r } from "@codemirror/legacy-modes/mode/r";
+import { shell } from "@codemirror/legacy-modes/mode/shell";
+import { yaml } from "@codemirror/legacy-modes/mode/yaml";
+
 import buildID from "components/utils/DebugIDUtil";
 
 import ids from "./ids";
-import viewerConstants from "./constants";
+import viewerConstants, { CODE_MIRROR_MODES } from "./constants";
 
 import Skeleton from "@material-ui/lab/Skeleton";
 
 export default function Editor(props) {
     const {
         baseId,
-        // mode = "",
+        mode = "",
         showLineNumbers,
         editable,
         wrapText,
@@ -33,6 +43,7 @@ export default function Editor(props) {
     const [wrapTextConfig] = useState(new Compartment());
     const [lineNumbersConfig] = useState(new Compartment());
     const [readOnlyConfig] = useState(new Compartment());
+    const [languageConfig] = useState(new Compartment());
     const [editorView, setEditorView] = useState();
     const [editorState] = useState(
         EditorState.create({
@@ -42,6 +53,7 @@ export default function Editor(props) {
                 wrapTextConfig.of(wrapText ? EditorView.lineWrapping : []),
                 lineNumbersConfig.of(showLineNumbers ? lineNumbers() : []),
                 readOnlyConfig.of(EditorState.readOnly.of(!editable)),
+                languageConfig.of([]),
                 EditorView.baseTheme({
                     "&": { height: viewerConstants.DEFAULT_VIEWER_HEIGHT },
                 }),
@@ -97,6 +109,42 @@ export default function Editor(props) {
             });
         }
     }, [editorView, readOnlyConfig, editable]);
+
+    useEffect(() => {
+        if (editorView) {
+            let lang;
+            switch (mode) {
+                case CODE_MIRROR_MODES.DOCKERFILE:
+                    lang = StreamLanguage.define(dockerFile);
+                    break;
+                case CODE_MIRROR_MODES.GITHUB_FLAVORED_MARKDOWN:
+                    lang = markdown();
+                    break;
+                case CODE_MIRROR_MODES.PERL:
+                    lang = StreamLanguage.define(perl);
+                    break;
+                case CODE_MIRROR_MODES.PYTHON:
+                    lang = python();
+                    break;
+                case CODE_MIRROR_MODES.R:
+                    lang = StreamLanguage.define(r);
+                    break;
+                case CODE_MIRROR_MODES.SHELL:
+                    lang = StreamLanguage.define(shell);
+                    break;
+                case CODE_MIRROR_MODES.YAML:
+                    lang = StreamLanguage.define(yaml);
+                    break;
+                default:
+                    lang = [];
+                    break;
+            }
+
+            editorView.dispatch({
+                effects: languageConfig.reconfigure(lang),
+            });
+        }
+    }, [editorView, languageConfig, mode]);
 
     return (
         <>

c3817cd0c96bfb11aeb90e7c7d7b4d933ec1dd56
sboleyn <sboleyn@outlook.com>
2023-04-07 12:47:34 -0700

    Update src/components/subscriptions/edit/EditSubAddons.js
    Co-authored-by: Paul Sarando <psarando@users.noreply.github.com>

--- src/components/subscriptions/edit/EditSubAddons.js
+++ src/components/subscriptions/edit/EditSubAddons.js
@@ -78,7 +78,7 @@ function EditSubAddonsDialog(props) {
                 announce({ text: t("subAddonsUpdated") });
                 onClose();
             },
-            onError: { setSubAddonUpdateError },
+            onError: setSubAddonUpdateError,
         }
     );
 

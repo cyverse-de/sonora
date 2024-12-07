3ed3123e87841a7a0cf5aea1ea95a2a4dcebfd03
Paul Sarando <psarando@cyverse.org>
2024-12-06 20:07:12 -0700

    Use next/dynamic to import `lottie-web` in LoadingAnimation
    Builds on my laptop are failing when trying to build SSR pages
    that use the LoadingAnimation, such as `/instantlaunch/[id]`.
    
    They are not failing in GitHub workflow skaffold builds,
    or on other dev machines, for now,
    but maybe this will prevent future build errors.

--- src/components/vice/loading/LoadingAnimation.js
+++ src/components/vice/loading/LoadingAnimation.js
@@ -4,12 +4,13 @@
 import React, { useEffect, useRef } from "react";
 
 import { makeStyles } from "tss-react/mui";
-import lottie from "lottie-web";
+import dynamic from "next/dynamic";
 import animation from "./vice_loading.json";
 
 import styles from "./styles";
 
 const useStyles = makeStyles()(styles);
+const lottie = dynamic(() => import("lottie-web"), { ssr: false });
 
 function LoadingAnimation() {
     let animationContainer = useRef();

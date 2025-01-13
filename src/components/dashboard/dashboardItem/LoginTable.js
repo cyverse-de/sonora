4121334e3cd0e2148e458ea5ddb88a6f15d562c6
Ian McEwen <mian@cyverse.org>
2025-01-13 13:04:54 -0700

    very initial stubbing of logins table on dashboard

---
+++ src/components/dashboard/dashboardItem/LoginTable.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * @author mian
+ *
+ * A table of recent logins for the user.
+ *
+ */
+
+import React from "react";
+
+import ErrorTypographyWithDialog from "components/error/ErrorTypographyWithDialog";
+import {
+    useTheme,
+    Skeleton,
+    Table,
+    TableContainer,
+    Paper,
+} from "@mui/material";
+
+export default function LoginsTable() {
+    const theme = useTheme();
+    var status = "loading";
+    var error = null;
+    if (status === "error") {
+        return (
+            <div style={{ padding: theme.spacing(1) }}>
+                <ErrorTypographyWithDialog
+                    errorObject={error}
+                    errorMessage={""}
+                />
+            </div>
+        );
+    }
+    if (status === "loading") {
+        return <Skeleton variant="rectangular" width={300} height={200} />;
+    }
+    return (
+        <TableContainer component={Paper}>
+            <Table size="small"></Table>
+        </TableContainer>
+    );
+}

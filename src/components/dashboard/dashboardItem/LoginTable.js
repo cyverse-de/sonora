509d7d72618632c4ab6f7c2d42c53a9e8001bcd9
Ian McEwen <mian@cyverse.org>
2025-01-15 16:12:10 -0700

    continue stubbing out logins table component

--- src/components/dashboard/dashboardItem/LoginTable.js
+++ src/components/dashboard/dashboardItem/LoginTable.js
@@ -13,7 +13,11 @@ import {
     useTheme,
     Skeleton,
     Table,
+    TableBody,
+    TableCell,
     TableContainer,
+    TableHead,
+    TableRow,
     Paper,
 } from "@mui/material";
 
@@ -21,6 +25,9 @@ export default function LoginsTable() {
     const theme = useTheme();
     var status = "loading";
     var error = null;
+    var data = {
+        logins: [{ ip_address: "0.0.0.0", login_time: 1736962713000 }],
+    };
     if (status === "error") {
         return (
             <div style={{ padding: theme.spacing(1) }}>
@@ -36,7 +43,22 @@ export default function LoginsTable() {
     }
     return (
         <TableContainer component={Paper}>
-            <Table size="small"></Table>
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="right">Login Time</TableCell>
+                        <TableCell align="right">IP Address</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data["logins"].map((row) => (
+                        <TableRow>
+                            <TableCell>{row["login_time"]}</TableCell>
+                            <TableCell>{row["ip_address"]}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </TableContainer>
     );
 }

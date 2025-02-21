53c57c4d2f011de858f941770fb3858022526fe7
Paul Sarando <psarando@cyverse.org>
2025-02-20 19:19:03 -0700

    CORE-1985 Remove fetch-mock, which conflicts with i18next in stories
    The fetch-mock responses were no longer needed in the Dashboard
    stories anyway, after 48caf028.

--- stories/UploadDropTarget.stories.js
+++ stories/UploadDropTarget.stories.js
@@ -5,24 +5,6 @@ import {
     useUploadTrackingState,
 } from "../src/contexts/uploadTracking";
 import { DataTableViewTest } from "./data/TableView.stories";
-import fetchMock from "fetch-mock";
-
-fetchMock.restore().post(/\/api\/upload\/?\??.*/, {
-    file: {
-        infoType: "",
-        path: "/iplant/home/ipcdev/lein-plugins.txt",
-        "share-count": 0,
-        "date-created": 1580918919000,
-        md5: "1deaf230acfa890bb0dabab537aff216",
-        permission: "own",
-        "date-modified": 1580918919000,
-        type: "file",
-        "file-size": 285,
-        label: "lein-plugins.txt",
-        id: "c5f7e42a-4831-11ea-a8b1-fa163e806578",
-        "content-type": "text/plain",
-    },
-});
 
 export const UploadDropTargetTest = ({ uploadsEnabled }) => {
     return (
@@ -39,23 +21,6 @@ const UnwrappedTest = ({ uploadsEnabled }) => {
     const incrementCompleted = () => {
         setUploadsCompleted((uploadsCompleted) => uploadsCompleted + 1);
     };
-
-    fetchMock.restore().post(/\/api\/upload\/?\??.*/, {
-        file: {
-            infoType: "",
-            path: "/iplant/home/ipcdev/lein-plugins.txt",
-            "share-count": 0,
-            "date-created": 1580918919000,
-            md5: "1deaf230acfa890bb0dabab537aff216",
-            permission: "own",
-            "date-modified": 1580918919000,
-            type: "file",
-            "file-size": 285,
-            label: "lein-plugins.txt",
-            id: "c5f7e42a-4831-11ea-a8b1-fa163e806578",
-            "content-type": "text/plain",
-        },
-    });
 
     return (
         <>

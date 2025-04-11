98709739a190966388c758dcd6f20bc4f790e4a1
Ian McEwen <mian@cyverse.org>
2025-04-11 13:44:52 -0700

    Start building out the form, using the useFormik approach for now

--- src/components/alerts/admin/AlertsAdmin.js
+++ src/components/alerts/admin/AlertsAdmin.js
@@ -4,7 +4,7 @@ import { useQueryClient, useMutation, useQuery } from "react-query";
 
 import withErrorAnnouncer from "components/error/withErrorAnnouncer";
 
-import { Skeleton } from "@mui/material";
+import { Skeleton, TextField } from "@mui/material";
 
 import { useTranslation } from "i18n";
 import { useFormik } from "formik";
@@ -16,6 +16,54 @@ import {
     addAlert,
     removeAlert,
 } from "serviceFacades/notifications";
+
+const initialValues = {
+    startDate: "",
+    endDate: "",
+    alertText: "",
+};
+
+const AddAlertForm = ({ t, handleSubmit }) => {
+    const formik = useFormik({
+        initialValues,
+        onSubmit: handleSubmit,
+    });
+
+    // i18n
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <TextField
+                variant="standard"
+                name="alertText"
+                label="Alert Text"
+                value={formik.values.alertText}
+                onChange={formik.handleChange}
+                error={formik.touched.alertText && formik.errors.alertText}
+                helperText={formik.touched.alertText && formik.errors.alertText}
+            />
+
+            <TextField
+                variant="standard"
+                name="endDate"
+                label="End Date"
+                value={formik.values.endDate}
+                onChange={formik.handleChange}
+                error={formik.touched.endDate && formik.errors.endDate}
+                helperText={formik.touched.endDate && formik.errors.endDate}
+            />
+
+            <TextField
+                variant="standard"
+                name="startDate"
+                label="Start Date (optional)"
+                value={formik.values.startDate}
+                onChange={formik.handleChange}
+                error={formik.touched.startDate && formik.errors.startDate}
+                helperText={formik.touched.startDate && formik.errors.startDate}
+            />
+        </form>
+    );
+};
 
 const AlertsEditor = ({ showErrorAnnouncer }) => {
     const isLoading = true;

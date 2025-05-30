psarando/cyverse-de-sonora

CORE-2050 Update data sharing to show announcer for `Basic` plans
Restores sharing buttons for unpaid `Basic` subscriptions,
but displays a warning announcer, with a link to the subscription page,
instead of opening the Sharing dialog.

src/components/sharing/SharingButton.js
@@ -12,10 +12,17 @@ import { Share } from "@mui/icons-material";
 
 import { useTranslation } from "i18n";
 import ids from "./ids";
+import announcePlanCannotShare from "./announcePlanCannotShare";
 
 function SharingButton(props) {
-    const { baseId, setSharingDlgOpen, size = "medium", margin = 1 } = props;
-    const { t } = useTranslation("sharing");
+    const {
+        baseId,
+        planCanShare,
+        setSharingDlgOpen,
+        size = "medium",
+        margin = 1,
+    } = props;
+    const { t } = useTranslation(["sharing", "subscriptions"]);
 
     return (
         <Button
@@ -24,7 +31,13 @@ function SharingButton(props) {
             disableElevation
             color="primary"
             size={size}
-            onClick={() => setSharingDlgOpen(true)}
+            onClick={() => {
+                if (planCanShare) {
+                    setSharingDlgOpen(true);
+                } else {
+                    announcePlanCannotShare(t);
+                }
+            }}
             startIcon={<Share />}
             style={{ margin }}
         >

src/components/sharing/SharingMenuItem.js
@@ -11,18 +11,24 @@ import { ListItemIcon, ListItemText, MenuItem } from "@mui/material";
 import { Share } from "@mui/icons-material";
 
 import ids from "./ids";
+import announcePlanCannotShare from "./announcePlanCannotShare";
 import { useTranslation } from "i18n";
 
 function SharingMenuItem(props) {
-    const { baseId, onClose, setSharingDlgOpen } = props;
-    const { t } = useTranslation("sharing");
+    const { baseId, planCanShare, onClose, setSharingDlgOpen } = props;
+    const { t } = useTranslation(["sharing", "subscriptions"]);
 
     return (
         <MenuItem
             id={buildID(baseId, ids.SHARING_MENU_ITEM)}
             onClick={() => {
                 onClose();
-                setSharingDlgOpen(true);
+
+                if (planCanShare) {
+                    setSharingDlgOpen(true);
+                } else {
+                    announcePlanCannotShare(t);
+                }
             }}
         >
             <ListItemIcon>

src/components/sharing/announcePlanCannotShare.js
@@ -1,0 +1,36 @@
+/**
+ * @author psarando
+ */
+
+import React from "react";
+
+import { Trans } from "i18n";
+import constants from "../../constants";
+
+import { WARNING } from "components/announcer/AnnouncerConstants";
+import { announce } from "components/announcer/CyVerseAnnouncer";
+import ExternalLink from "components/utils/ExternalLink";
+
+/**
+ * An announcer to display a message when a subscription does not allow sharing.
+ *
+ * @param t - The i18n translation function returned by `useTranslation`.
+ *            The `subscriptions` namespace is required.
+ */
+const announcePlanCannotShare = (t) =>
+    announce({
+        text: (
+            <Trans
+                t={t}
+                i18nKey="subscriptions:sharingNotAvailable"
+                components={{
+                    subscriptionLink: (
+                        <ExternalLink href={constants.SUBSCRIBE_URL} />
+                    ),
+                }}
+            />
+        ),
+        variant: WARNING,
+    });
+
+export default announcePlanCannotShare;

cyverse-de/sonora

src/components/sharing/SharingButton.js
@@ -12,10 +12,17 @@ import { Share } from "@mui/icons-material";
 
 import { useTranslation } from "i18n";
 import ids from "./ids";
+import announcePlanCannotShare from "./announcePlanCannotShare";
 
 function SharingButton(props) {
-    const { baseId, setSharingDlgOpen, size = "medium", margin = 1 } = props;
-    const { t } = useTranslation("sharing");
+    const {
+        baseId,
+        planCanShare,
+        setSharingDlgOpen,
+        size = "medium",
+        margin = 1,
+    } = props;
+    const { t } = useTranslation(["sharing", "common"]);
 
     return (
         <Button
@@ -24,7 +31,13 @@ function SharingButton(props) {
             disableElevation
             color="primary"
             size={size}
-            onClick={() => setSharingDlgOpen(true)}
+            onClick={() => {
+                if (planCanShare) {
+                    setSharingDlgOpen(true);
+                } else {
+                    announcePlanCannotShare(t);
+                }
+            }}
             startIcon={<Share />}
             style={{ margin }}
         >

src/components/sharing/SharingMenuItem.js
@@ -11,18 +11,24 @@ import { ListItemIcon, ListItemText, MenuItem } from "@mui/material";
 import { Share } from "@mui/icons-material";
 
 import ids from "./ids";
+import announcePlanCannotShare from "./announcePlanCannotShare";
 import { useTranslation } from "i18n";
 
 function SharingMenuItem(props) {
-    const { baseId, onClose, setSharingDlgOpen } = props;
-    const { t } = useTranslation("sharing");
+    const { baseId, planCanShare, onClose, setSharingDlgOpen } = props;
+    const { t } = useTranslation(["sharing", "common"]);
 
     return (
         <MenuItem
             id={buildID(baseId, ids.SHARING_MENU_ITEM)}
             onClick={() => {
                 onClose();
-                setSharingDlgOpen(true);
+
+                if (planCanShare) {
+                    setSharingDlgOpen(true);
+                } else {
+                    announcePlanCannotShare(t);
+                }
             }}
         >
             <ListItemIcon>

src/components/sharing/announcePlanCannotShare.js
@@ -1,0 +1,36 @@
+/**
+ * @author psarando
+ */
+
+import React from "react";
+
+import { Trans } from "i18n";
+import constants from "../../constants";
+
+import { WARNING } from "components/announcer/AnnouncerConstants";
+import { announce } from "components/announcer/CyVerseAnnouncer";
+import ExternalLink from "components/utils/ExternalLink";
+
+/**
+ * An announcer to display a message when a subscription does not allow sharing.
+ *
+ * @param t - The i18n translation function returned by `useTranslation`.
+ *            The `common` namespace is required.
+ */
+const announcePlanCannotShare = (t) =>
+    announce({
+        text: (
+            <Trans
+                t={t}
+                i18nKey="common:sharingNotAvailable"
+                components={{
+                    subscriptionLink: (
+                        <ExternalLink href={constants.SUBSCRIBE_URL} />
+                    ),
+                }}
+            />
+        ),
+        variant: WARNING,
+    });
+
+export default announcePlanCannotShare;